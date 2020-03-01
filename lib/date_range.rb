15f25f7b10a22feb3bf76093649863f62e3cf3d2
SuelyBarreto <suely@barreto.us>
2020-03-01 15:41:40 -0800

    completed test and class for date range

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -3,18 +3,36 @@ module Hotel
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
+      unless start_date.class == Date && end_date.class == Date
+        raise ArgumentError, "Parameters must be of class Date"
+      end
+      if start_date >= end_date
+        raise ArgumentError, "Start date must be before end date"
+      end
+      @start_date = start_date
+      @end_date   = end_date
     end
 
     def overlap?(other)
-      return false
+      unless other.class == Hotel::DateRange
+        raise ArgumentError, "Parameters must be of class Hotel::DateRange"
+      end
+
+      return other.start_date < @end_date && other.end_date > @start_date
+
+      # return !(other.start_date >= @end_date || other.end_date <= @start_date)
     end
 
     def include?(date)
-      return false
+      unless date.class == Date
+        raise ArgumentError, "Parameters must be of class Date"
+      end
+
+      return date >= @start_date && date < @end_date
     end
 
     def nights
-      return 3
+      return (@end_date - @start_date).to_i
     end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -1,85 +1,159 @@
 require_relative "test_helper"
 
 describe "Hotel::DateRange" do
-  describe "consructor" do
+
+  describe "constructor" do
+
     it "Can be initialized with two dates" do
       start_date = Date.new(2017, 01, 01)
-      end_date = start_date + 3
-
+      end_date   = start_date + 3
       range = Hotel::DateRange.new(start_date, end_date)
 
       expect(range.start_date).must_equal start_date
       expect(range.end_date).must_equal end_date
     end
 
-    xit "is an error for negative-length ranges" do
+    it "is an error if parameters are not dates" do
+      start_date = "2017/01/01"
+      end_date   = "2017/01/04"
+
+      expect{ range = Hotel::DateRange.new(start_date, end_date) }.must_raise ArgumentError
     end
 
-    xit "is an error to create a 0-length range" do
+    it "is an error for negative-length ranges" do
+      start_date = Date.new(2017, 01, 01)
+      end_date   = start_date - 3
+     
+      expect{ range = Hotel::DateRange.new(start_date, end_date) }.must_raise ArgumentError
     end
+
+    it "is an error to create a 0-length range" do
+      start_date = Date.new(2017, 01, 01)
+      end_date   = start_date
+     
+      expect{ range = Hotel::DateRange.new(start_date, end_date) }.must_raise ArgumentError
+    end
+
   end
 
   describe "overlap?" do
+
     before do
-      start_date = Date.new(2017, 01, 01)
-      end_date   = start_date + 3
+
+      def simple_range(start_day, end_day)
+        return Hotel::DateRange.new(Date.new(2017, 01, start_day), Date.new(2017, 01, end_day))
+      end
+
+    end
+
+    it "raises an ArgumentError if parameter is not a range" do
+      range1 = simple_range(01, 05)
+      range2 = Date.new(2017, 01, 01)
+      expect{ range1.overlap?(range2) }.must_raise ArgumentError
+    end
+
+    it "returns true for the same range" do
+      range1 = simple_range(01, 05)
+      range2 = simple_range(01, 05)
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns true for a contained range" do
+      range1 = simple_range(05, 10)
+      range2 = simple_range(01, 15)
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns true for a range that overlaps in front" do
+      range1 = simple_range(05, 15)
+      range2 = simple_range(01, 10)
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns true for a range that overlaps in the back" do
+      range1 = simple_range(01, 10)
+      range2 = simple_range(05, 15)
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns true for a containing range" do
+      range1 = simple_range(01, 15)
+      range2 = simple_range(05, 10)
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns false for a range starting on the end_date date" do
+      range1 = simple_range(01, 05)
+      range2 = simple_range(05, 10)
+      expect(range1.overlap?(range2)).must_equal false
+    end
+
+    it "returns false for a range ending on the start_date date" do
+      range1 = simple_range(05, 10)
+      range2 = simple_range(01, 05)
+      expect(range1.overlap?(range2)).must_equal false
+    end
+
+    it "returns false for a range completely before" do
+      range1 = simple_range(01, 05)
+      range2 = simple_range(10, 15)
+      expect(range1.overlap?(range2)).must_equal false
+    end
+
+    it "returns false for a date completely after" do
+      range1 = simple_range(10, 15)
+      range2 = simple_range(01, 05)
+      expect(range1.overlap?(range2)).must_equal false
+    end
+
+  end
+
+  describe "include?" do
+
+    before do
+      start_date = Date.new(2018, 01, 01)
+      end_date   = Date.new(2018, 01, 10)
 
       @range = Hotel::DateRange.new(start_date, end_date)
     end
 
-    it "returns true for the same range" do
-      start_date = @range.start_date
-      end_date   = @range.end_date
-      test_range = Hotel::DateRange.new(start_date, end_date)
-
-      expect(@range.overlap?(test_range)).must_equal true
+    it "raises an ArgumentError if parameter is not a date" do
+      expect{ @range.include?("2018/01/01") }.must_raise ArgumentError
     end
 
-    xit "returns true for a contained range" do
-    end
-
-    xit "returns true for a range that overlaps in front" do
-    end
-
-    xit "returns true for a range that overlaps in the back" do
-    end
-
-    xit "returns true for a containing range" do
-    end
-
-    xit "returns false for a range starting on the end_date date" do
-    end
-
-    xit "returns false for a range ending on the start_date date" do
-    end
-
-    xit "returns false for a range completely before" do
-    end
-
-    xit "returns false for a date completely after" do
-    end
-  end
-
-  xdescribe "include?" do
     it "returns false if the date is clearly before" do
+      expect(@range.include?(Date.new(2017, 01, 01))).must_equal false
     end
 
     it "returns false if the date is clearly after" do
+      expect(@range.include?(Date.new(2019, 01, 01))).must_equal false
     end
 
     it "returns true for dates in the range" do
+      expect(@range.include?(Date.new(2018, 01, 05))).must_equal true
     end
 
     it "returns true for the start_date date" do
+      expect(@range.include?(Date.new(2018, 01, 01))).must_equal true
     end
 
     it "returns false for the end_date date" do
+      expect(@range.include?(Date.new(2018, 01, 10))).must_equal false
     end
 
   end
 
-  xdescribe "nights" do
+  describe "nights" do
+
     it "returns the correct number of nights" do
+      start_date = Date.new(2017, 01, 01)
+      end_date   = start_date + 5
+      range = Hotel::DateRange.new(start_date, end_date)
+
+      expect(range.nights).must_be_kind_of Integer
+      expect(range.nights).must_equal 5
     end
+
   end
+
 end
