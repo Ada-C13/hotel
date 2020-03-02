919217f5a6b9c0d70431b78aa86507ce20c729e6
Nataliya Pogodina <n.pogodina@gmail.com>
2020-03-02 12:40:07 -0800

    Created Room class and test file for it.

---
+++ test/room_test.rb
@@ -0,0 +1,14 @@
+require_relative 'test_helper'
+
+describe "Room Class" do
+  describe "Room instantiation" do
+    before do
+      @room = Hotel::Room.new()
+    end
+
+    it "is an instance of Room" do
+      expect(@room).must_be_kind_of Hotel::Room
+    end
+  end
+
+end
