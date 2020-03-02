be9f6d6ba223e1fd814875f9d5cc28e5054153dc
Jessica Stone <jstone4815@gmail.com>
2020-03-02 15:03:48 -0800

    add tests for HotelManager list_rooms method

--- test/hotel_manager_test.rb
+++ test/hotel_manager_test.rb
@@ -1,11 +1,33 @@
 require_relative "test_helper"
 
 describe "HotelManager" do
+  before do
+    @hotel_manager = Hotel::HotelManager.new
+  end
+
   describe "initialize" do
     it "creates an instance of HotelManager" do
-      hotel_manager = Hotel::HotelManager.new
+      expect(@hotel_manager).must_be_kind_of Hotel::HotelManager
+    end
+  end
 
-      expect(hotel_manager).must_be_kind_of Hotel::HotelManager
+  describe "list_rooms" do
+    it "returns an array" do
+      @hotel_manager.rooms.push("this is a room")
+      rooms = @hotel_manager.list_rooms
+
+      expect(rooms).must_be_kind_of Array
+    end
+
+    it "contains Room objects within the array" do
+      #TODO after Room class has been created
+    end
+
+    it "returns nil if there are no rooms" do
+      @hotel_manager.rooms.empty?()
+      rooms = @hotel_manager.list_rooms
+
+      expect(rooms).must_be_nil
     end
   end
 end
