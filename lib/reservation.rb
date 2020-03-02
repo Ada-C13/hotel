c55bccd0b5d0f27491c372957970dc24b90241c2
thenora <thenora@gmail.com>
2020-03-02 15:21:19 -0800

    included framework from Ada

---
+++ lib/reservation.rb
@@ -0,0 +1,10 @@
+module Hotel
+  class Reservation
+    def initialize(start_date, end_date, room)
+    end
+
+    def cost
+      return 3
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,18 @@
-# Add simplecov
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "minitest/skip_dsl"
 require "simplecov"
 SimpleCov.start do
   add_filter 'test/' # Tests should not be checked for coverage.
 end
 
+require "date"
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
-
-
-
-require_relative "../lib/room"
+require_relative "../lib/room.rb"
+require_relative "../lib/hotel_controller.rb"
+require_relative "../lib/reservation.rb"
+require_relative "../lib/date_range.rb"
