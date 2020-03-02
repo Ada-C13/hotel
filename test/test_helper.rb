70dcbcc97e40c0d85cacb89a17c3046b6e30b68a
thenora <thenora@gmail.com>
2020-03-02 13:40:27 -0800

    added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,16 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "simplecov"
+SimpleCov.start do
+  add_filter 'test/' # Tests should not be checked for coverage.
+end
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+
+
+
+require_relative "../lib/room
+"
