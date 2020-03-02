a7768b9efc9fb9b9dbbd72e3c2b0a8117a06d84f
Olga Patrakova <olgapatrakova@outlook.com>
2020-03-02 14:35:04 -0800

    Add Reservation class with basic initialize method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,5 +1,15 @@
+require_relative 'room'
+
 module Hotel
-  class Room
-    attr_reader
+  class Reservation
+    attr_reader :id, :room, :room_number, :guest, :start_date, :end_date
+
+    def initialize(id, room_number, guest, start_date, end_date)
+      @id = id
+      @room_number = room_number
+      @guest = guest
+      @start_date = start_date
+      @end_date = end_date
+    end
   end
 end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -1,1 +1,10 @@
 require_relative 'test_helper'
+
+describe "Reservation class" do
+  describe "#initialize" do
+    it "creates an instance of Reservation class" do
+      booking = Hotel::Reservation.new(1, 1, "test guest", "test start", "test end")
+      expect(booking).must_be_instance_of Hotel::Reservation
+    end
+  end
+end
