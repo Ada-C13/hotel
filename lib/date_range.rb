baf302b4b9b0d3a36529b821d435f0583ca40197
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-02 12:40:58 -0800

    Edited test_helper file to include tests, began date_range logic

---
+++ lib/date_range.rb
@@ -0,0 +1,20 @@
+require 'date'
+
+module Hotel
+  class DateRange
+    attr_reader :check_in, :check_out
+    def initialize(check_in, check_out)
+      @check_in = Date.new(check_in) unless !Date.valid_date?(check_in)
+      @check_out = Date.new(check_out) unless !Date.valid_date?(check_out)
+      @duration = Integer.new
+      @days = []
+
+      @days = find_days(check_in, check_out)
+    end
+
+    def find_days(start, end)
+      #take the start and the end, if start.next.to_s is same as end.next.to_s, total_days gets [Date.new(start), Date.new(end)]. Else, do some logic to 
+      return total_days
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,15 @@
-# Add simplecov
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require 'date'
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/'
+end
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+require_relative 'date_range'
+require_relative 'hotel_block'
+require_relative 'reception'
+require_relative 'reservation'
