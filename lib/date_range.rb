249018629ac03374e565c1fea2d3cfdf9db61d72
Nikki Vaughan <vnikki13@gmail.com>
2020-03-02 15:06:08 -0800

    made reader methods for attributes

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -2,6 +2,8 @@ require 'Date'
 
 module Hotel
   class DateRange
+    attr_reader :start_date, :end_date
+    
     def initialize(start_date, end_date)
       @start_date = start_date
       @end_date = end_date
