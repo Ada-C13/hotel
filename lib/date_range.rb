4f527cbbb51f9ced4fefebafb51c523e05575148
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-02 15:14:54 -0800

    Added tests to date_range_test file and some logic to date_range to initialize and check that check in time and check out time are valid

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -2,19 +2,37 @@ require 'date'
 
 module Hotel
   class DateRange
-    attr_reader :check_in, :check_out
-    def initialize(check_in, check_out)
-      @check_in = Date.new(check_in) unless !Date.valid_date?(check_in)
-      @check_out = Date.new(check_out) unless !Date.valid_date?(check_out)
-      @duration = Integer.new
+    attr_reader :check_in_time, :check_out_time, :days
+    def initialize(check_in_time, check_out_time)
+      @check_in_time = Date.new(check_in_time[0], check_in_time[1], check_in_time[2])
+      @check_out_time = Date.new(check_out_time[0], check_out_time[1], check_out_time[2])
       @days = []
 
-      @days = find_days(check_in, check_out)
+      if @check_in_time.class != Date
+        raise ArgumentError, "The check in time #{check_in_time} is not a valid time."
+      elsif @check_out_time.class != Date
+        raise ArgumentError, "The check out time #{check_out_time} is not a valid time."
+      elsif (@check_out_time - @check_in_time) <= 0
+        raise ArgumentError, "The dates are not valid. Check in: #{@check_in_time}, Check out: #{@check_out_time}"
+
+      end
+
+      # @days = create_days(@check_in_time, @check_out_time)
+      # @duration = days.length
     end
 
-    def find_days(check_in, check_out)
-      #take the start and the end, if start.next.to_s is same as end.next.to_s, total_days gets [Date.new(start), Date.new(end)]. Else, do some logic to 
-      return total_days
-    end
+    # def create_days
+    #   # total_days = Array.new
+    #   # if @check_in_time.next == @check_out_time
+    #   #   total_days = [@check_in_time, @check_out_time]
+    #   # end
+    #   # #take the start and the end, if start.next.to_s is same as end.next.to_s, total_days gets [Date.new(start), Date.new(end)]. Else, do some logic to 
+    #   # # return total_days
+    #   # return total_days
+    # end
+
+    # def nights
+    #   return duration - 1
+    # end
   end
 end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -1,1 +1,33 @@
 require_relative 'test_helper'
+
+describe 'initialize' do
+  before do
+    @date = Hotel::DateRange.new([2020, 1, 28], [2020, 1, 29])
+  end
+  it "can be created" do
+    expect(@date).must_be_instance_of Hotel::DateRange
+  end
+  it "will have check in and check out times that are Date objects" do
+    expect(@date.check_in_time).must_be_instance_of Date
+    expect(@date.check_out_time).must_be_instance_of Date
+  end
+  it "will not create instance if check out time is before check in time" do
+    expect {
+      date = Hotel::DateRange.new([2020, 1, 28], [2020, 1, 27])
+    }.must_raise ArgumentError
+  end
+end
+
+describe 'create_days method' do
+  before do
+    @date = Hotel::DateRange.new([2020, 1, 28], [2020, 1, 30])
+  end
+  it "will assess the correct number of days" do
+    expect(@date.days.length).must_equal 3
+  end
+  it "will create an array of Date objects" do
+    expect(@date.days).must_be_instance_of Array
+    expect(@date.days[0]).must_be_instance_of Date
+  end
+
+end
