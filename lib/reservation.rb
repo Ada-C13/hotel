808a723134fed2a9b5c5769d6f07611ac92b68f2
Nataliya Pogodina <n.pogodina@gmail.com>
2020-03-02 15:28:17 -0800

    Added Reservation class and its tests.

---
+++ lib/reservation.rb
@@ -0,0 +1,14 @@
+require "date"
+
+module Hotel
+  class Reservation
+    attr_reader :start_date, :end_date
+
+    def initialize(start_date, end_date)
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
+    end
+  end
+
+
+end
---
+++ test/reservation_test.rb
@@ -0,0 +1,22 @@
+require_relative "test_helper"
+
+describe "Reservation class" do
+  describe "Reservation instatiation" do
+    before do
+      @start_date = "2020-4-1"
+      @end_date = "2020-4-5"
+      @reservation = Hotel::Reservation.new(@start_date, @end_date)
+    end
+
+    it "is an instance of reservation" do
+      expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+
+    it "stores start and end dates as Date objects" do
+      expect(@reservation.start_date).must_be_kind_of Date
+    end
+
+  end
+
+
+end
