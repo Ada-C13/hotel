b6acc8742b8ef02594cd85875465ca0f90942188
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-02 15:48:56 -0800

    Cleaned up unnecessary comments and passed all tests

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -14,7 +14,7 @@ module Hotel
         raise ArgumentError, "The check out time #{check_out_time} is not a valid time."
       elsif (@check_out_time <=> @check_in_time) <= 0
         raise ArgumentError, "The dates are not valid. Check in: #{@check_in_time}, Check out: #{@check_out_time}"
-      else #just create the shit
+      else
         date = @check_in_time.clone
         until date === @check_out_time.next
           @days << date
@@ -22,18 +22,7 @@ module Hotel
         end
       end
     end
-
-    # def create_days
-    #   if (@check_in_time + 1) == @check_in_time
-    #     return [@check_in_time, @check_out_time]
-    #   else
-    #     total_days = Array.new
-        
-    #   end
-    #   #take the start and the end, when start.next.to_s is same as end.next.to_s, total_days gets [Date.new(start), Date.new(end)]. Else, do some logic to 
-    #   # return total_days
-    # end
-
+    
     def nights
       return days.length - 1
     end
