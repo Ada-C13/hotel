3517883eab5b735e1c26804557e832e56e3cb881
Nikki Vaughan <vnikki13@gmail.com>
2020-03-02 15:49:52 -0800

    required path to hotel_controller file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative "../lib/date_range"
 require_relative "../lib/reservation"
+require_relative "../lib/hotel_controller"
