b28f321d4d0702a09937bec4cfe4ac1105afed9e
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.localdomain>
2020-03-02 13:58:09 -0800

    I add files that i create in lib

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
+# Add simplecov
 require 'simplecov'
 SimpleCov.start do
   add_filter 'test/' # Tests should not be checked for coverage.
 end
 
-# Add simplecov
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
@@ -11,3 +12,8 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+
+require_relative '../lib/data_range'
+require_relative '../lib/hotel_controller'
+require_relative '../lib/reservation'
+
