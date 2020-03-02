40e66fb94b0c05f388c405ec4fc8835bcd6963b0
Stephanie <stephaniejmars@gmail.com>
2020-03-02 15:20:00 -0800

    added room initialize & tests

--- lib/room.rb
+++ lib/room.rb
@@ -5,15 +5,27 @@ require_relative 'reservation'
 
 module Hotel
   class Room
-    attr_reader
+    attr_reader :rm_num, :cost
     attr_writer
-    attr_accessor
+    attr_accessor 
 
-    # def initialize 
+    def initialize(rm_num:, cost: 200)
+      @rm_num = rm_num
+      @cost = cost
 
-    # end
+      if rm_num.nil? || rm_num <= 0 
+        raise ArgumentError, 'Room number cannot be blank or less than one.'
+      end
+
+      if cost <= 0 
+        raise ArgumentError, 'Room number cannot be blank or less than one.'
+      end
+    end
+
+
 
   end
 end
 
 YAYME = 1
+
--- test/room_test.rb
+++ test/room_test.rb
@@ -12,4 +12,37 @@ describe "room" do
     YAYME.must_equal 1
   end 
 
+  describe "initialze" do 
+    it "creates an instance of Room" do 
+      room = Hotel::Room.new(rm_num: 3)
+      room.must_be_kind_of Hotel::Room
+    end 
+
+    it "verifies rm_num is valid" do
+    rm_num = 3
+    room = Hotel::Room.new(rm_num: rm_num, cost: 300)
+    room.must_respond_to :rm_num
+    room.rm_num.must_be_instance_of Integer
+    room.rm_num.must_equal rm_num
+    end
+
+    it "verifies cost is valid" do 
+    cost = 300
+    room = Hotel::Room.new(rm_num: 3, cost: cost)
+    room.must_respond_to :cost
+    room.cost.must_be_instance_of Integer
+    room.cost.must_equal cost
+    end
+
+    it "raises argument error if invalid rm_num" do 
+      expect{Hotel::Room.new(rm_num: -3)}.must_raise ArgumentError
+      expect{Hotel::Room.new(rm_num: nil)}.must_raise ArgumentError
+    end 
+
+    it "raises argument error if invalid cost" do 
+      expect{Hotel::Room.new(rm_num: 3, cost: -200)}.must_raise ArgumentError
+    end 
+    
+  end 
+
 end
