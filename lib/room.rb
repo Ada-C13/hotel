14c712fb250851dd46e2c95975e9a4fe5d8f280d
Jessica Stone <jstone4815@gmail.com>
2020-03-03 14:47:05 -0800

    add check_availability method to Room class

--- lib/room.rb
+++ lib/room.rb
@@ -4,8 +4,24 @@ module Hotel
     attr_writer :cost
 
     def initialize(reservations = [], number, cost)
+      @reservations = []
       @number = number
       @cost = cost
+    end
+
+    def check_availability(daterange)
+      if @reservations.empty?
+        return true
+      else
+        reservations.each do |reservation|
+          if (reservation.range[-1] == daterange.range[0])
+            next
+          elsif (reservation.range & daterange.range).any?
+            return false
+          end
+        end
+        return true
+      end
     end
 
   end
