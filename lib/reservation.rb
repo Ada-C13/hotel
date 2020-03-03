b6a5f716aadcaddf9fcf713a839b75aa0d2f9e5f
thenora <thenora@gmail.com>
2020-03-02 17:35:15 -0800

    Added initialize to reservation & test

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,10 +1,17 @@
 module Hotel
   class Reservation
-    def initialize(start_date, end_date, room)
+    # attr_reader :id
+    # ! should I add a reservation id?
+    attr_accessor :start_date, :end_date, :room, :cost
+
+
+    # ? what's the difference between "variable," as shown below, and "variable:,"
+    def initialize(start_date, end_date, room, cost = 200)
+      @start_date = start_date
+      @end_date = end_date
+      @room = room
+      @cost = cost
     end
 
-    def cost
-      return 3
-    end
   end
 end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -1,6 +1,16 @@
 require_relative "test_helper"
 
 describe Hotel::Reservation do
+  before do
+    @reservation = Hotel::Reservation.new(start_date = Date.new(2021, 01, 01), end_date = start_date + 3, room = 2, cost = 200)
+  end
+
+  describe "initialize" do
+    it "is an instance of Reservation" do
+      expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+  end
+
   describe "cost" do
     it "returns a number" do
       start_date = Date.new(2017, 01, 01)
