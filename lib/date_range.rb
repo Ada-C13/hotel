65a70fa69341bff23fc39b6f56408644d0720877
Vera <lizeth.veraro@outlook.com>
2020-03-03 14:33:51 -0800

    Including Self.validate_date in data_range to validate data format

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,9 +1,12 @@
+require 'date'
+
 module Hotel
   class DateRange
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
-
+      self.class.validate_date(start_date)
+      self.class.validate_date(end_date)
     end
 
     def overlap?(data_range)
@@ -17,5 +20,13 @@ module Hotel
     def nights
       return 3
     end
+
+
+    private
+    
+    def self.validate_date(date)
+      return true
+    end
+
   end
 end
