c2b7018e9541dc8c59f2ddcbe95cce820d8b52a6
Jessica Liang <jessicursliang@gmail.com>
2020-03-02 16:07:40 -0800

    add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
-# Add simplecov
+
 require 'date'
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require 'simplecov'
+SimpleCov.start
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
