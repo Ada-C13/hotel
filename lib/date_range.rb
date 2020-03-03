f7664ec795b25bcc54e83718c8577449aec1ab88
Katie <katie.vandervoort@gmail.com>
2020-03-03 15:55:32 -0800

    added initialize method

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,8 +1,13 @@
+require 'date'
+
 module Hotel
   class DateRange
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
+      raise ArgumentError.new("start date has to be at least 1 day before end date") if (end_date - start_date).to_i < 1
+      @start_date = start_date
+      @end_date = end_date
     end
 
     def overlap?(other)
