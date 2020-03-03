c9f3f538a44e8b2f916c00e36b5f1828d134e394
Nikki Vaughan <vnikki13@gmail.com>
2020-03-02 17:00:04 -0800

    Raises ArgumentError if dates are not valid and started method to list all reservations

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -2,8 +2,12 @@ module Hotel
   class HotelController
 
     def self.reserve_room(start_date, end_date)
+      raise ArgumentError, "End date must be after start date" if end_date < start_date || end_date == start_date
       return Reservation.new(start_date, end_date)
     end
 
+    def self.list_reservations(start_date, end_date = start_date, room = nil)
+      
+    end
   end
 end
