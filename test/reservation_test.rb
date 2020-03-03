d92ed09ab233e3e629efe695b202b5e40ff7875a
Jessica Stone <jstone4815@gmail.com>
2020-03-03 12:35:49 -0800

    add test for creating Reservation class

---
+++ test/reservation_test.rb
@@ -0,0 +1,19 @@
+require_relative "test_helper"
+
+describe "Reservation" do
+  before do
+    @reservation = Hotel::Reservation.new(Date.new(2020,5,3), Date.new(2020,5,5))
+  end
+
+  describe "initialize" do
+    it "creates an instance of Reservation" do
+      expect(@reservation).must_be_kind_of Hotel::Reservation
+      expect(@reservation).must_respond_to :start_date
+      expect(@reservation).must_respond_to :end_date
+      expect(@reservation).must_respond_to :nights
+      expect(@reservation).must_respond_to :cost
+      expect(@reservation).must_respond_to :id
+      expect(@reservation).must_respond_to :room_number
+    end
+  end
+end
