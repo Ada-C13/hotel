4048b4f6d7cae708dcc914e062d09a38d2be2bed
Yieni <yaynee8d@gmail.com>
2020-03-03 11:53:16 -0800

    fixed various bugs from changing filenames

--- test/daterange_test.rb
+++ test/daterange_test.rb
@@ -1,5 +1,10 @@
 require_relative 'test_helper'
 
-describe "daterange" do
+describe "DateRange" do
+  describe "initialize" do
+    it "raises ArgumentError if end date is before start date" do
+      expect{Hotel::DateRange.new(start_date: "3/4/2020", end_date: "3/3/2020")}.must_raise ArgumentError
+    end
+  end
   
 end
--- test/reservations_test.rb
+++ test/reservations_test.rb
@@ -3,20 +3,15 @@ require_relative 'test_helper'
 describe "reservations" do
   describe "initialize reservations" do
     before do
-      @new_reservation = Hotel::Reservation.new( id: 1, start_date: "3/4/2020", end_date: "3/9/2020", room: Hotel::Room.new(room_number: 1, cost: 200))
+      @new_reservation = Hotel::Reservation.new( id: 1, date_range: Hotel::DateRange.new(start_date: "3/1/2020", end_date: "3/4/2020"), room: Hotel::Room.new(room_number: 1, cost: 200))
     end
 
     it "creates an instance of a reservation" do
       expect(@new_reservation).must_be_instance_of Hotel::Reservation
     end
 
-    it "converts start and end dates to instances of Date class" do
-      expect(@new_reservation.start_date).must_be_instance_of Date
-      expect(@new_reservation.end_date).must_be_instance_of Date
-    end
-
-    it "raises ArgumentError if end date is before start date" do
-      expect{Hotel::Reservation.new( id: 1, start_date: "3/4/2020", end_date: "3/3/2020")}.must_raise ArgumentError
+    it "has an attribute of date_range that is an instance of Date_Range class" do
+      expect(@new_reservation.date_range).must_be_instance_of Hotel::DateRange
     end
 
     it "has an attribute of room that is an instance of Room class" do
