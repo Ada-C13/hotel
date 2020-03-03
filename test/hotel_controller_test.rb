94dd0d48724d9add88da4e56f12464c167f7dac9
Nikki Vaughan <vnikki13@gmail.com>
2020-03-02 17:01:09 -0800

    Add test cases for methods that will be implemented in the HotelController class

--- test/hotel_controller_test.rb
+++ test/hotel_controller_test.rb
@@ -1,15 +1,44 @@
 require_relative 'test_helper'
 
 describe 'HotelController class' do 
-  describe 'HotelController' do
+  describe 'HotelController making reservations' do
+    before do
+      @start_date = Date.new(1993, 2, 24)
+      @end_date = Date.new(1993, 2, 26)
+      @reservation = Hotel::HotelController.reserve_room(@start_date, @end_date)
+    end
+
+    it 'makes a reservation with start and end dates' do
+      expect(@reservation).must_be_instance_of Hotel::Reservation
+    end
+
+    it 'raises an Error if there are not two seperate dates given' do
+      same_date = Date.new(1993, 2, 24)
+      expect{Hotel::HotelController.reserve_room(@start_date, same_date)}.must_raise ArgumentError
+    end
+
+    it 'raises an Error if the end date is before the start date' do
+      expect{Hotel::HotelController.reserve_room(@end_date, @start_date)}.must_raise ArgumentError
+    end
+  end
+
+  describe 'HotelController listing reservations' do
     before do
       start_date = Date.new(1993, 2, 24)
       end_date = Date.new(1993, 2, 26)
       @reservation = Hotel::HotelController.reserve_room(start_date, end_date)
     end
 
-    it 'makes a reservation with start and end dates' do
-      expect(@reservation).must_be_instance_of Hotel::Reservation
+    it 'lists all rooms in hotel' do
+
+    end
+
+    it 'lists reservations for specific room' do
+      Hotel::HotelController.list_reservations(@start_date)
+    end
+
+    it 'lists reservations for specific date range or single date given' do
+      
     end
   end
 end
