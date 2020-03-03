326034fba33e99bdd80ee5ae231b43112ca89be5
cojenco <icathyouyang@gmail.com>
2020-03-03 13:42:35 -0800

    Revised Reservation class to pass in date_range as parameter. Revised reservation_test.

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,22 +2,21 @@ require_relative 'date_range'
 
 module Hotel
   class Reservation
-    attr_reader :id, :start_date, :end_date, :date_range, :room_id
+    attr_reader :id, :start_date, :end_date, :date_range, :room_id, :hotel_block_id
 
     @@next_id = 1
 
-    def initialize(start_date, end_date, room_id)  
-      raise ArgumentError.new("Please pass in Date class instances.") if !(start_date.is_a? Date) || !(end_date.is_a? Date)
-      raise ArgumentError.new("Live in the present! Dates should not be prior to today.") if start_date < Date.today
-      raise ArgumentError.new("End date should not be earlier than or the same day as start date.")  if end_date <= start_date
-      @start_date = start_date
-      @end_date = end_date
-      @date_range = DateRange.new(start_date, end_date)
+    def initialize(date_range, room_id)  
+      @date_range = date_range
+      @start_date = date_range.start_date
+      @end_date = date_range.end_date
+    
       @room_id = room_id
 
       @id = @@next_id
       @@next_id += 1
-      # @hotel_block_id
+
+      @hotel_block_id = -1
     end
 
     def  get_price
@@ -29,3 +28,22 @@ module Hotel
 
   end
 end
+
+
+# attr_reader :id, :start_date, :end_date, :date_range, :room_id
+
+# @@next_id = 1
+
+# def initialize(start_date, end_date, room_id)  
+#   raise ArgumentError.new("Please pass in Date class instances.") if !(start_date.is_a? Date) || !(end_date.is_a? Date)
+#   raise ArgumentError.new("Live in the present! Dates should not be prior to today.") if start_date < Date.today
+#   raise ArgumentError.new("End date should not be earlier than or the same day as start date.")  if end_date <= start_date
+#   @start_date = start_date
+#   @end_date = end_date
+#   @date_range = DateRange.new(start_date, end_date)
+#   @room_id = room_id
+
+#   @id = @@next_id
+#   @@next_id += 1
+#   # @hotel_block_id
+# end
--- lib/system_coordinator.rb
+++ lib/system_coordinator.rb
@@ -13,13 +13,17 @@ module Hotel
 
     # # many methods
     # # method to make reservation
-    # def make_reservation(date_range)
-    # end
+    # def make_reservation(start_date, end_date)
 
     # [method] track reservation by date to find list of reservations by date
+    # def find_reservation_date (date_range)
+    # def find_reservation_date (start_date, end_date)
+
     # [method] access the list of reservations for a specified room and a given date range
+    # def find_reservation_room_date(room_id, date_range)
     
     # [method] avail rooms for that day. I can view a list of rooms that are not reserved for a given date range
+    # def find_availabile_rooms(date_range)
 
     # handle/ rescue exceptions when there is no availability
 
