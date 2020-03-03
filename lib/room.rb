d8d71ea62b65e6c5a4e833ccbe130c047f6fcf70
denisseai <msdenisseanaya@gmail.com>
2020-03-03 15:12:42 -0800

    Added more tests for Room regarding cost and id

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,18 @@
+
 module Hotel
   class Room
-    attr_reader :id
+    ROOMS = 20
+    attr_reader :id, :nightly_rate, :reservations
 
     def initialize(id)
+      if id <= 0 || id > 20
+        raise ArgumentError.new("Not a real room")
+      end
+
       @id = id
+      @nightly_rate = 200
+      @reservations = []
     end
+
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -5,6 +5,33 @@ describe "Room" do
     it "Create instance of Room" do
       room = Hotel::Room.new(1)
       expect(room).must_be_kind_of Hotel::Room
-    end    
+    end 
+
+    it "Room id tracker" do
+      room = Hotel::Room.new(1)
+      expect(room).must_respond_to :id
+    end
+
+    it "Rooms numbers 1 - 20 only" do
+      expect(Hotel::Room.new(1).id).must_equal 1
+      expect(Hotel::Room.new(20).id).must_equal 20
+    end
+
+    it "Raises an ArgumentError if id is outside of 1 - 20" do
+      expect{Hotel::Room.new(0)}.must_raise ArgumentError
+      expect{Hotel::Room.new(21)}.must_raise ArgumentError
+    end
+
+    it "Same nightly cost for all" do
+      room = Hotel::Room.new(1)
+      expect(room.nightly_rate).must_equal 200
+    end
+
+    it "Reservations is an empty array" do
+      room = Hotel::Room.new(1)
+
+      expect(room.reservations).must_be_kind_of Array
+      expect(room.reservations).must_equal []
+    end   
   end
 end
