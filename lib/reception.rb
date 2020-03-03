7abc587f0d09b80c39a0c32108ac59ea3892c5fd
Alicia Combs <alicia.combs.92@gmail.com>
2020-03-02 17:15:34 -0800

    Updated reservation and reception class, started filling out tests for each

--- lib/reception.rb
+++ lib/reception.rb
@@ -1,23 +1,40 @@
 module Hotel
   class Reception
     attr_reader :rooms, :reservations
+
     def initialize
       @rooms = []
-      @reservations
+      @reservations = []
       20.times do |i|
         i += 1
-        @rooms << {room_id: i, cost: 200}
+        @rooms << {room_id: i}
       end
     end
-    def list_reservs_by_room_date(room_id, date)
-      requested_date = Date.new(date[0], date[1], date[2])
-      reservations.select { |reservation| reservation.room == room_id && reservation.date_range.days.include?(requested_date)}
+
+    def reservations_by_room_date(room_id, date)
+      date = Date.new(date[0], date[1], date[2]) unless date.class == Date
+      return reservations.select { |reservation| reservation.room == room_id && reservation.date_range.days.include?(date)}
     end
-    def list_reservs_by_date
-      
+
+    def reservations_by_date(date)
+      date = Date.new(date[0], date[1], date[2]) unless date.class == Date
+      return reservations.select { |reservation| reservation.date_range.days.include?(date) }
     end
+
     def find_reservation(id)
       return reservations.find { |reservations| reservations.id == id }
+    end
+
+    def available_rooms
+      reserved_rooms = reservations.map { |reservation| reservation.room_id }
+      return rooms.difference(reserved_rooms)
+    end
+
+    def make_reservation(check_in_time, check_out_time)
+      #find free room
+      room_id = available_rooms.first
+      @reservations << Reservation.new(check_in_time, check_out_time, room_id)
+      return reservations.last
     end
   end
 end
--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,15 +1,13 @@
+require_relative 'date_range'
+
 module Hotel
   class Reservation
-    attr_reader :check_in_time, :check_out_time, :room, :date_range
-    def initialize(check_in_time, check_out_time, room: 1)
-      @id = 
+    attr_reader :id, :room_id, :date_range
+    def initialize(check_in_time, check_out_time, room_id)
+      @id = rand(111111..999999)
       @date_range = Hotel::DateRange.new(check_in_time, check_out_time)
-      @room
-      @cost = @date_range.
-      #logic to find room
+      @room_id = room_id
+      @cost = @date_range.nights * 200
     end
-
-
-
   end
 end
