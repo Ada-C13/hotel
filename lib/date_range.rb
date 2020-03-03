5516ce2724977d19bed12cf9ef400a319c37d54d
Hannah <hannahjoo24@gmail.com>
2020-03-03 15:55:40 -0800

    Deleted DateRange#valid_dates?

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -8,19 +8,26 @@ module Hotel
     def initialize(start_date, end_date)
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
-    end 
-
-
-    def valid_dates?
+      
       if @start_date > @end_date
         raise ArgumentError, "Start date must not be after end date"
       end 
-      
+
       range = Date.today..Date.new(2020, 12, 31)
 
       if !(range.cover?(@start_date) && range.cover?(@end_date))
         raise ArgumentError, "Date range must be between #{Date.today} and #{Date.new(2020, 12, 31)}"
       end 
+    end 
+
+
+    def valid_dates?
+      
+      # range = Date.today..Date.new(2020, 12, 31)
+
+      # if !(range.cover?(@start_date) && range.cover?(@end_date))
+      #   raise ArgumentError, "Date range must be between #{Date.today} and #{Date.new(2020, 12, 31)}"
+      # end 
     end
 
     def nights 
