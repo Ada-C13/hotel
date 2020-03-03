f57b318293bcaa539c3019ac2a1c9eee4afb68f9
Stephanie <stephaniejmars@gmail.com>
2020-03-03 13:59:10 -0800

    method create_rooms added with test

--- lib/room.rb
+++ lib/room.rb
@@ -5,27 +5,35 @@ require_relative 'reservation'
 
 module Hotel
   class Room
-    attr_reader :rm_num, :cost
+    attr_reader :rm_num
     attr_writer
     attr_accessor 
 
-    def initialize(rm_num:, cost: 200)
+    def initialize(rm_num:)
       @rm_num = rm_num
-      @cost = cost
+     
 
       if rm_num.nil? || rm_num <= 0 
         raise ArgumentError, 'Room number cannot be blank or less than one.'
       end
 
-      if cost <= 0 
-        raise ArgumentError, 'Room number cannot be blank or less than one.'
-      end
+      # if cost <= 0 
+      #   raise ArgumentError, 'Room number cannot be blank or less than one.'
+      # end
     end
 
 
+    def self.create_rooms(num)
+      all_rooms = []
+      i = 1
+      num.times do 
+        all_rooms << Room.new(rm_num: i)
+        i + 1
+      end
+      return all_rooms
+    end
+  
+    
 
   end
 end
-
-YAYME = 1
-
