d6abc6ea0cbbad24888152eac07139873ca187f4
cojenco <icathyouyang@gmail.com>
2020-03-02 21:01:52 -0800

    Added DateRange#overlapping method and tests. Revised #including method and tests.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -3,13 +3,13 @@ require 'date'
 module Hotel
   class DateRange
     
-    attr_reader :start_date, :end_date
+    attr_accessor :start_date, :end_date
 
     # your library code should assume that it's receiving Date objects to start
     def initialize(start_date, end_date)
-      raise ArgumentError.new("Arguments passed in should be a Date class instance ") if !(start_date.is_a? Date) || !(end_date.is_a? Date)
-      raise ArgumentError.new("Dates should not be earlier than today") if start_date < Date.today
-      raise ArgumentError.new("End date should not be earlier than start date")  if end_date <= start_date
+      raise ArgumentError.new("Please pass in Date class instances.") if !(start_date.is_a? Date) || !(end_date.is_a? Date)
+      raise ArgumentError.new("Live in the present! Dates should not be prior to today.") if start_date < Date.today
+      raise ArgumentError.new("End date should not be earlier than or the same day as start date.")  if end_date <= start_date
       
       @start_date = start_date
       @end_date = end_date
@@ -20,15 +20,15 @@ module Hotel
       return nights
     end
 
-    def include(date)
-      if date >= @start_date && date < @end_date
+    def including(other_range)
+      if other_range.start_date >= @start_date && other_range.end_date <= @end_date
         return true
       else
         return false
       end
     end
 
-    def overlap(other_range)
+    def overlapping(other_range)
       if other_range.start_date < @end_date && @start_date < other_range.end_date
         return true
       else
@@ -36,14 +36,5 @@ module Hotel
       end
     end
 
-
   end
 end
-
-
-# def initialize(start_year:, start_month:, start_day:, end_year:, end_month:, end_day:)
-#   @start_date = Date.new(start_year, start_month, start_day)
-#   @end_date = Date.new(end_year, end_month, end_day)
-
-#   raise ArgumentError.new("End date should not be earlier than start date")  if @end_date < @start_date
-# end
--- test/daterange_test.rb
+++ test/daterange_test.rb
@@ -4,21 +4,19 @@ describe "DateRange Class" do
   before do
     start_date = Date.today + 5
     end_date = Date.today + 10
-  
     @range01 = Hotel::DateRange.new(start_date, end_date)
   end
 
   describe "Initialize" do
-    
-    it "Is an instance of DateRange" do
+    it "is an instance of DateRange" do
       expect(@range01).must_be_kind_of Hotel::DateRange
     end
 
-    it "Stores a start date that is a Date class" do
+    it "stores a start date that is a Date class" do
       expect(@range01.start_date).must_be_kind_of Date
     end
 
-    it "Stores an end date that is a Date class" do
+    it "stores an end date that is a Date class" do
       expect(@range01.end_date).must_be_kind_of Date
     end
 
@@ -31,8 +29,8 @@ describe "DateRange Class" do
     it "raises ArgumentError if start_date or end_date is not a Date class" do
       start_date = Date.today + 10
       end_date = Date.today + 15
-      expect{Hotel::DateRange.new("2020/03/25", end_date)}.must_raise ArgumentError
-      expect{Hotel::DateRange.new(start_date, 20200327)}.must_raise ArgumentError
+      expect{Hotel::DateRange.new("2025/03/25", end_date)}.must_raise ArgumentError
+      expect{Hotel::DateRange.new(start_date, 20250327)}.must_raise ArgumentError
     end
 
     it "raises ArgumentError if start_date is in the past" do
@@ -56,34 +54,48 @@ describe "DateRange Class" do
     end
   end
 
-  describe "#include" do
-    it "returns true if the date is included in the date range" do
-      day01 = Date.today + 7
-      expect(@range01.include(day01)).must_equal true
+  describe "#including" do
+    it "returns true if the other date range is included in the date range" do
+      range21 = Hotel::DateRange.new(Date.today + 7, Date.today + 8)
+      expect(@range01.including(range21)).must_equal true
+      range23 = Hotel::DateRange.new(Date.today + 5, Date.today + 6)
+      expect(@range01.including(range23)).must_equal true
     end
 
-    it "returns false if the date is not included in the date range" do
-      day02 = Date.today + 1
-      expect(@range01.include(day02)).must_equal false
+    it "returns false if the other date range is not included in the date range" do
+      range22 = Hotel::DateRange.new(Date.today + 1, Date.today + 2)
+      expect(@range01.including(range22)).must_equal false
+      range24 = Hotel::DateRange.new(Date.today + 8, Date.today + 20)
+      expect(@range01.including(range24)).must_equal false
+      range26 = Hotel::DateRange.new(Date.today + 10, Date.today + 11)  #edge case for checkout date
+      expect(@range01.including(range26)).must_equal false
+      range28 = Hotel::DateRange.new(Date.today + 4, Date.today + 5) #edge case for checkout date
+      expect(@range01.including(range28)).must_equal false
     end
   end
 
-  describe "#overlap" do
+  describe "#overlapping" do
     it "returns true if the two date ranges overlap" do
-      start_date = Date.today + 6
-      end_date = Date.today + 8
-      range03 = Hotel::DateRange.new(start_date, end_date)
-      expect(@range01.overlap(range03)).must_equal true
+      range03 = Hotel::DateRange.new(Date.today + 6, Date.today + 8)
+      expect(@range01.overlapping(range03)).must_equal true
+      range04 = Hotel::DateRange.new(Date.today + 5, Date.today + 6)
+      expect(@range01.overlapping(range04)).must_equal true
+      range05 = Hotel::DateRange.new(Date.today+8, Date.today+20)
+      expect(@range01.overlapping(range05)).must_equal true
     end
 
-    it "returns false if it is only the start date overlaps end date" do
-      start_date = Date.today + 10
-      end_date = Date.today + 12
-      range04 = Hotel::DateRange.new(start_date, end_date)
-      expect(@range01.overlap(range04)).must_equal false
+    it "returns false if the two date ranges do not overlap" do
+      range06 = Hotel::DateRange.new(Date.today + 1, Date.today + 3)
+      expect(@range01.overlapping(range06)).must_equal false
+      range09 = Hotel::DateRange.new(Date.today + 100, Date.today + 103)
+      expect(@range01.overlapping(range09)).must_equal false
+    end
+
+    it "returns false if it is only the start date overlapping end date" do
+      range07 = Hotel::DateRange.new(Date.today + 10, Date.today + 12) #edge case for checkout date
+      expect(@range01.overlapping(range07)).must_equal false
+      range08 = Hotel::DateRange.new(Date.today + 1, Date.today + 5) #edge case for checkout date
+      expect(@range01.overlapping(range08)).must_equal false
     end
   end
 end
-
-
-
