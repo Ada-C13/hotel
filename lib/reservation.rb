9ec6a03ae0804e1ec8247a7b5da3f11d088dd62f
Jessica Stone <jstone4815@gmail.com>
2020-03-03 12:36:02 -0800

    create Reservation class with initialize method

---
+++ lib/reservation.rb
@@ -0,0 +1,18 @@
+require 'date'
+require_relative 'date_range'
+
+module Hotel
+  class Reservation < DateRange
+    attr_reader :start_date, :end_date, :id, :cost, :nights, :room_number
+
+    def initialize(start_date, end_date)
+      super(start_date, end_date)
+
+      @id = id
+      @total_cost = total_cost
+      @nights = nights
+      @room_number = room_number
+    end
+
+  end
+end
