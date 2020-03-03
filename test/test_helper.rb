84888ec09345b55c66d538abbfbbce69bed8a28e
Catherina <catherine.pchelina@gmail.com>
2020-03-02 16:12:12 -0800

    Modified test_helper.rb by adding require_relative(s)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,8 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require 'minitest/skip_dsl'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# require_relative your lib files here!
+require_relative '../lib/date_range'
