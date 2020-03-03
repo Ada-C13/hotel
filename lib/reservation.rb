867519e384b7be46ed57d9753186e170eeff413f
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-03 13:15:08 -0800

    Add class Reservation, create instance of Reservation, add tests - tests passing

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,24 +1,50 @@
+require 'date'
+
 module Hotel
   class Reservation
-    attr_reader :id, :start_date, :end_date, :room, :cost
+    attr_reader :res_id, :start_date, :end_date
 
     def initialize(
       # these are mandatory parameters for Reservation.new
-      id:, 
+      res_id:, 
       start_date:, 
-      end_date:, 
-      room:
+      end_date:
+      
     )
-      # these are instance variables and differ from each instance
-      @id = id
-      @start_date = start_date
-      @end_date = end_date
-      @room = room
-      @cost = 200
+      # these are instance variables
+      @res_id = res_id
+      @total_cost
 
+
+      if start_date.is_a?(String)
+        start_date = Date.parse(start_date)
+      end
+      @start_date = start_date
+    
+      if end_date.is_a?(String)
+        end_date = Date.parse(end_date)
+      end
+      @end_date = end_date
 
     end #initialize end
+
+     def connect(room)
+      @room = room
+      room.add_trip(self)
+    end
 
   end #class Reservation end
   
 end #HotelController end
+
+
+
+
+
+# Date.parse('2001-02-03') ??
+
+# make method to calculate total cost of reservation. cost * nights
+
+# i need total num of nights. so subtract start date from end date  and (subtract one) for the checkout day. 
+
+# create loop to 
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -3,21 +3,37 @@ require_relative 'test_helper'
 describe "Reservation class" do
   describe "initialize" do
     before do
+      #Date.parse("September 3") # -> 2015-09-03
+      start_date= "March 1, 2019"
+      end_date = "March 5, 2019"
       @reservation_data = Hotel::Reservation.new(
-        id: 1337,
-        start_date: "January 9, 2020",
-        end_date: "January 11, 2020",
-        room: 19
+        res_id: 1337,
+        start_date: start_date,
+        end_date: end_date
     )
-    end #before end
+    end #before do end
 
     it "is an instance of Reservation" do
       expect(@reservation_data).must_be_kind_of Hotel::Reservation
+      # puts "This is the actual id #{@reservation_data.id}" 
+      puts "This is the res_id #{@reservation_data.res_id}"
     end #Instance of Reservation end
     
-    it ""
+    it "has a start_date that is a Date object" do
+      expect(@reservation_data.start_date).must_equal Date.parse("March 1, 2019") 
+    end
+
+    it "has an end_date that is a Date object" do
+      expect(@reservation_data.end_date).must_equal Date.parse("March 5, 2019")
+    end
+
+
   end #Describe initialize end
 
 
 end #Reservation class describe block end
 
+
+# DATE constants which are arrays of month names and day names
+# Date::MONTHNAMES # (index 0 = nil)
+# Date::DAYNAMES starts on Sunday. use .rotate(1) to change to Monday. 
