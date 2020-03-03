51141d2e728dc3cabfa6876446a4a9d5c94da7e5
Nikki Vaughan <vnikki13@gmail.com>
2020-03-02 16:58:42 -0800

    started methods to find a room and calculate total cost of reservations

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,11 +2,22 @@ require_relative 'date_range'
 
 module Hotel
   class Reservation < DateRange
-    attr_reader :start_date, :end_date, :room
+    attr_reader :arrive, :depart, :room_id
 
-    def initialize(start_date, end_date)
-      super(start_date, end_date)
-      @room
+    def initialize(arrive, depart)
+      super(arrive, depart)
+      @room_id = find_room
+      @nights = Hotel::DateRange.nights(arrive, depart)
+      @total_cost = 
+    end
+
+
+    def self.find_room
+      
+    end
+
+    def self.total_cost
+
     end
   end
 end
