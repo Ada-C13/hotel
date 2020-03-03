27f6e87e300a5477524f3558e6c09a4ad394b601
Hannah <hannahjoo24@gmail.com>
2020-03-03 11:11:40 -0800

    Updated test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,13 @@
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
+require "date"
+
+require 'simplecov'
+SimpleCov.start do 
+  add_filter 'test/'
+end 
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
-require_relative "../lib/hotel"
