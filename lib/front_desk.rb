5c77079a8ed0ea0f9f69dca2123e7336bffeab1c
Vera <lizeth.veraro@outlook.com>
2020-03-03 14:35:13 -0800

    Method  to generate the rooms, 1 to 20 numbers of room

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -3,6 +3,18 @@ module Hotel
   class FrontDesk
     attr_reader :rooms, :datarange
 
+    def initialize
+      @rooms = generate_rooms
+    end
+  
+    def generate_rooms
+      hotel_rooms = Array.new
+        (1..20).each do |room_id|
+          hotel_rooms << Room.new(room_id)
+      end
+      return hotel_rooms
+    end
+
     def room_list
       return []
     end
