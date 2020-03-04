d9c8db702553043821305e147124902082e8084b
Katie <katie.vandervoort@gmail.com>
2020-03-03 16:01:55 -0800

    created file and initialize method for room

---
+++ lib/room.rb
@@ -0,0 +1,14 @@
+module Hotel
+  class Room
+    attr_reader :room_number, :cost
+    attr_accessor :reservations_for_room
+
+    def initialize(room_number, cost: 200)
+      raise ArgumentError if room_number.class != Integer
+      @room_number = room_number
+      @cost = cost
+      reservations_for_room = []
+    end
+
+  end
+end
