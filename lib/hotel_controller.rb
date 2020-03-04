d4269932aa5794e9395bb854a88e46f13f1439c7
Katie <katie.vandervoort@gmail.com>
2020-03-03 16:10:37 -0800

    added initialize method to hotel controller

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -1,6 +1,24 @@
+require 'date'
+
+require_relative 'reservation.rb'
+require_relative 'date_range.rb'
+require_relative 'room.rb'
+
 module Hotel
   class HotelController
-    # Wave 1
+    attr_reader :rooms, :reservations
+
+    def initialize
+      rooms_array = []
+      20.times{ |i| rooms_array.push(Hotel::Room.new(i+1)) }
+      @rooms = rooms_array
+      @reservations = []
+    end
+
+    def list_rooms
+      return @rooms
+    end
+    
     def rooms
       # You might want to replace this method with an attr_reader
       return []
