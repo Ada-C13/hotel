0fa2588c6832c876cd0d26261a3df965b19f95a8
Kate Mangubat <kate.d.mangubat@gmail.com>
2020-03-03 23:34:27 -0800

    updated classes for wave 1, added methods

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,51 +1,118 @@
 require 'awesome_print'
 require 'date'
 require 'csv'
+require_relative 'room'
 
 module Hotel
     class Reservation
-        attr_reader :reservation_id, :check_in, :check_out, :number_of_days
-        attr_accessor :status, :room_id
-        VALID_STATUS = [:pending, :denied, :confirmed]
+      attr_reader :reservation_id, :check_in, :check_out, :number_of_days
+      attr_accessor :status, :room_number, :room
+      VALID_STATUS = [:pending, :denied, :confirmed]
 
-        def initialize(reservation_id, check_in, number_of_days, status = :pending, room_id = nil)
-            @reservation_id = reservation_id
-            @check_in = check_in
-            @number_of_days = number_of_days
-            @status = status
-            @room_id = room_id
+      def initialize(reservation_id, check_in, number_of_days, status = :pending, room = nil, room_number = nil)
+        @reservation_id = reservation_id
+        @check_in = check_in
+        @number_of_days = number_of_days
+        @status = status
+        @check_out = check_in + number_of_days
 
-            @check_out = check_in + number_of_days
+        if room
+          @room = room
+          @room_number = room.room_number
+        elsif room_number
+          @room_number = room_number
+        else
+          raise ArgumentError, 'Room or room_number is required'
         end
 
-        def self.load_all_reservations () #what to get
-            reservations = CSV.parse(File.read(__dir__ + "/../support/reservations.csv"), headers: true, header_converters: :symbol,) #relative to HOTEL & reading csv file
-            all_reservations = [] #save all rooms to this array
-              reservations.each do |record| #loop through each record in reservations csv
-                reservation_id = record[:reservation_id]
-                check_in = Date.parse(record[:check_in])
-                number_of_days = record[:number_of_days].to_i
-                status = record[:status].to_sym
-                room_id = record[:room_id]
-                temp_reservation = Reservation.new(reservation_id, check_in, number_of_days, status, room_id)
-                all_reservations.push(temp_reservation) #push Room into all_rooms
-              end
-          return all_reservations #return all the rooms
+
+        if number_of_days < 0
+            raise ArgumentError.new("Invalid, number of days must be greater than 0")
+        end
+      end #initialize
+
+      #Total cost for a given reservation
+      #returns total_cost (float)
+      def total_cost()
+        #TODO check if room exists
+        total_cost = @room.price.to_f * @number_of_days
+        return total_cost
+      end  
+
+        #list of reservations for a specific date
+      def get_reservations(date) 
+        list_of_reservations_date = []
+        @reservations.each do |reservation|
+          if check_in == reservation.check_in
+            list_of_reservations_date.push(reservation)   
           end
+        end
+        return list_of_reservations_date
+      end
 
-    end
-end
+      def self.load_all_reservations() #what to get
+        reservations = CSV.parse(File.read(__dir__ + "/../support/reservations.csv"), headers: true, header_converters: :symbol) #relative to HOTEL & reading csv file
+        all_reservations = [] #save all rooms to this array
+        reservations.each do |record| #loop through each record in reservations csv
+          reservation_id = record[:reservation_id]
+          check_in = Date.parse(record[:check_in])
+          number_of_days = record[:number_of_days].to_i
+          status = record[:status].to_sym
+          room_number = record[:room_number]
 
-# date = Date.parse("10/10/2020")
-#  date = Date.parse("2020-02-01")
-#  puts date
-# test = Hotel::Reservation.new(1, date, 30, "confirmed", 1)
+          temp_reservation = Reservation.new(reservation_id, check_in, number_of_days, status,nil, room_number)
+          all_reservations.push(temp_reservation) #push Room into all_rooms
+        end
+        return all_reservations #return all the rooms
+      end
 
+      # checks if aDate is within the check_in and check_out
+      # aDate (Date)
+      # returns true or false 
+      def is_within_stay(aDate)
+        if (aDate >= @check_in) && (aDate < @check_out)
+          return true
+        end
+        return false
+      end
+
+          # #valid date
+    # def validate_date(date)
+    #   if date.class == String
+    #     date = Date.parse(date)
+    #   elsif date.class != Date
+    #     raise ArgumentError.new("You entered #{date}. Invalid, try again using this format '2020-03-04'")
+    #   end
+    #   return date
+    # end
+
+    # #validate stay
+    # def validate_stay(check_in, number_of_days)
+    #   check_in = validate_date(check_in)
+    #   check_out = validate_date(check_out)
+    #   if check_in > check_out
+    #     raise ArgumentError.new("Invalid. This check-out date: #{check_out} is before the check-in date: #{check_in}")
+    #   end
+    # end
+
+    end #class
+end #module
+
+
+
+
+date = Date.parse("2020-02-01")
+# puts date
+room = Hotel::Room.new("10A",200)
+res = Hotel::Reservation.new(1, date, 7, "confirmed", room)
+puts res.is_within_stay(Date.parse("2020-02-01"))
+
+puts res.total_cost
 # puts test.reservation_id
 # puts test.check_in
 # puts test.number_of_days
 # puts test.status
-# puts test.room_id
+# puts test.room_number
 # puts test.check_out
 
 # test = Hotel::Reservation.new(1, date, 10)
@@ -54,7 +121,7 @@ end
 # puts test.check_in
 # puts test.number_of_days
 # puts test.status
-# puts test.room_id
+# puts test.room_number
 # puts test.check_out
 # Hotel::Reservation.load_all_reservations.each do |res|
 #     puts res.reservation_id
--- lib/reservation_system.rb
+++ lib/reservation_system.rb
@@ -6,15 +6,102 @@ require_relative 'reservation'
 
 
 module Hotel
-    class ReservationSystem
-      attr_reader :rooms, :reservations
+  class ReservationSystem
+    attr_reader :rooms, :reservations
         
-      def initialize()
-        @rooms = Room.load_all_rooms()
-        @reservations = Reservation.load_all_reservations()
-      end
+    def initialize()
+      @rooms = Room.load_all_rooms()
+      @reservations = Reservation.load_all_reservations()
     end
-  end
 
-  test = Hotel::ReservationSystem.new
-  puts test.rooms
+    #Search thru reservations and find ones that match room and date
+    # aRoom_number (string)
+    # date (Date)
+    # returns Array of Reservations
+    def find_reservations(aDate, aRoom_number = nil) 
+      reservations = []
+      # search through reservations
+      @reservations.each do |reservation|
+        # check if requested aRoom_number is same as reservation's room id
+        # and requested date is within the reservation's  checkin to checkout timeframe
+        if aRoom_number != nil
+          if (aRoom_number == reservation.room_number) && (reservation.is_within_stay(aDate))
+            # if both are true, save the reservation  
+              reservations.push(reservation)
+          end
+        else
+          #checking rooms with this date only
+          if (reservation.is_within_stay(aDate))
+              reservations.push(reservation)
+          end
+        end
+      end
+      return reservations
+    end
+
+    # find room based on id
+    # returns Room
+    def find_room(aRoom_number)
+      found_room = nil
+      @rooms.each do |room|
+        if room.room_number == aRoom_number
+          return room
+        end
+      end
+      return found_room
+    end
+
+    #TODO
+    # Reserves a room based on reservation request
+    # aReservation (Reservation) - the reservation request
+    # returns aReservation with its status updated to :confirmed or :denied
+    def reserve_room(aReservation)
+      #loop through rooms
+      #check if room is available for reservation dates
+      
+      return aReservation
+    end
+
+  end # Class
+end # Module
+
+
+
+    #unavailable rooms
+
+    #available rooms
+    
+    #reserve room
+
+    #cancel room
+
+    #reservation for given date
+
+    #reservation for date range
+
+    #validate room
+
+    #assign room
+
+    #validate date again?
+
+
+
+  # #Wave 1
+  # test = Hotel::ReservationSystem.new
+  # #Access list of all the rooms in the hotel
+  # puts test.rooms
+  # #Access list of reservations for specified room and a given date range
+  # date = Date.today
+  # res = Hotel::ReservationSystem.getReservations(date, 5)
+
+  hotelsystem = Hotel::ReservationSystem.new
+  puts hotelsystem.rooms.class
+  puts hotelsystem.reservations.class
+
+  found_res = hotelsystem.find_reservations(Date.parse("2020-10-29"))
+  found_res.each do |res|
+    puts res.room_number
+    puts res.check_in
+    puts res.check_out
+  end
