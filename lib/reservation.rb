6617737b3f601338973c86700ae309205694df09
Katie <katie.vandervoort@gmail.com>
2020-03-03 16:16:16 -0800

    updated initialize method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,7 +1,11 @@
 module Hotel
   class Reservation
-    # Feel free to change this method signature as needed. Make sure to update the tests!
     def initialize(start_date, end_date, room)
+      # raise ArgumentError.new("room must be an instance of room") if room.class != Hotel::Room
+      @start_date = start_date
+      @end_date = end_date
+      @room = room # instance of room class
+      @date_range = Hotel::DateRange.new(start_date, end_date)
     end
 
     def cost
