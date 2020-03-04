3c7ca28ba30ebc986717d491dfd6061f49f85bfd
Katie <katie.vandervoort@gmail.com>
2020-03-03 16:07:14 -0800

    added tests for reservations to test if the reservations method would return a list of actual reservations

--- test/hotel_controller_test.rb
+++ test/hotel_controller_test.rb
@@ -12,14 +12,21 @@ describe Hotel::HotelController do
         expect(rooms).must_be_kind_of Array
       end
     end
-    describe "reserve_room" do
-      it "takes two Date objects and returns a Reservation" do
-        start_date = @date
-        end_date = start_date + 3
+    describe "reservations" do
+      before do
+        @hotel_controller.reserve_room(Date.new(2020,01,01), Date.new(2020,01,05))
+        @hotel_controller.reserve_room(Date.new(2020,02,01), Date.new(2020,02,05))
+        @hotel_controller.reserve_room(Date.new(2020,03,01), Date.new(2020,03,05))
+        @hotel_controller.reserve_room(Date.new(2020,03,01), Date.new(2020,03,10))
+        @hotel_controller.reserve_room(Date.new(2020,03,04), Date.new(2020,03,06))
+        @reservation_list = @hotel_controller.reservations(@date)
+      end
 
-        reservation = @hotel_controller.reserve_room(start_date, end_date)
-
-        expect(reservation).must_be_kind_of Hotel::Reservation
+      it "takes a Date and returns a list of Reservations" do
+        expect(@reservation_list).must_be_kind_of Array
+        @reservation_list.each do |res|
+          res.must_be_kind_of Hotel::Reservation
+        end
       end
     end
 
