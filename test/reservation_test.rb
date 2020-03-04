34620f7c20078804dbc0f2ffa9acf161cf4de55c
Nikki Vaughan <vnikki13@gmail.com>
2020-03-03 22:05:09 -0800

    dummy test that sees if a new reservation is initialized with the right information

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -3,13 +3,19 @@ require_relative 'test_helper'
 describe 'Reservation class' do 
   describe 'Reservation instantiation' do
     before do 
-      start_date = Date.new(1993, 2, 24)
-      end_date = Date.new(1993, 2, 26)
-      @reservation = Hotel::Reservation.new(start_date, end_date)
+      arrive = Date.new(1993, 2, 24)
+      depart = Date.new(1993, 2, 26)
+      @reservation = Hotel::Reservation.new(arrive, depart)
     end
 
     it 'makes a reservation with start and end dates' do
       expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+
+    it 'contains room number, nights staying, and total cost of stay' do
+      expect(@reservation.room_num).must_be_kind_of Integer
+      expect(@reservation.nights).must_equal 2
+      expect(@reservation.total_cost).must_equal 0
     end
   end
 end
