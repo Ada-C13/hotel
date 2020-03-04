3c75bc409c872be7d1c056c0a5d989db869faa28
SuelyBarreto <suely@barreto.us>
2020-03-03 20:39:47 -0800

    adding tests to hotel controller tests

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -1,49 +1,49 @@
 module Hotel
   class HotelController
     # Access the list of all of the rooms in the hotel
-    attr_reader :rooms, :reservation_list
+    attr_reader :rooms, :reservations
     
     # Constructor
     # Hotel has 20 rooms, numbered 1 through 20
     def initialize
       @rooms = (1..20).to_a
-      @reservation_list = []
+      @reservations = []
     end
 
     # Reserve a Room for a Given Date Range
     # Given only start and end dates, determine which room to use for the reservation
     def reserve_room(start_date, end_date)
-      room = available_rooms(start_date, end_date, room).first
+      room = available_rooms(start_date, end_date).first
       reservation = Hotel::Reservation.new(start_date, end_date, room)
-      @reservation_list << reservation
+      @reservations << reservation
       return reservation
     end
 
     # Access the list of reservations for a specific date, so that I can track reservations by date
-    def reservations(date)
-      return @reservation_list.select do |reservation|
+    def reservations_by_date(date)
+      return @reservations.select do |reservation|
         reservation.range.include?(date)
       end
     end
 
     # Returns Reservation(s) Specific for Room and Range
     # Access the list of reservations for a specified room and a given date range
-    def reservation_by_room(start_date, end_date, room)
+    def reservations_by_room(start_date, end_date, room)
       range = Hotel::DateRange.new(start_date, end_date)
-      return @reservation_list.select do |reservation| # which reservations have an overlap with the ranged passed
+      return @reservations.select do |reservation| # which reservations have an overlap with the ranged passed
         reservation.range.overlap?(range) && reservation.room == room
       end
     end
 
     # Returns Specific Room Available for a Date Range
-    def room_available(start_date, end_date, room)
-      return reservation_by_room(start_date, end_date, room).size == 0
+    def is_room_available?(start_date, end_date, room)
+      return reservations_by_room(start_date, end_date, room).size == 0
     end
 
     # Returns List of Rooms Available for a Date Range
     # View a list of rooms that are not reserved for a given date range, so that I can see all available rooms for that date range
-    def available_rooms(start_date, end_date, room)
-      return @rooms.select { |room| room_available(start_date, end_date, room) }
+    def available_rooms(start_date, end_date)
+      return @rooms.select { |room| is_room_available?(start_date, end_date, room) }
     end
 
   end
--- test/hotel_controller_test.rb
+++ test/hotel_controller_test.rb
@@ -2,47 +2,67 @@ require_relative "test_helper"
 
 describe "Hotel::HotelController" do
   before do
-    @hotel_controller = Hotel::HotelController.new
-    @date = Date.parse("2020-08-04")
+    @hotel = Hotel::HotelController.new
+    def test_date(day)
+      return Date.new(2020, 01, day)
+    end
   end
 
   describe "wave 1" do
-    
     describe "rooms" do
       it "returns a list" do
-        expect(@hotel_controller.rooms).must_be_kind_of Array
+        expect(@hotel.rooms).must_be_kind_of Array
       end
 
       it "return 20 rooms" do
-        expect(@hotel_controller.rooms.size).must_equal 20
+        expect(@hotel.rooms.size).must_equal 20
       end
 
       it "return 20 rooms numbered 1 through 20" do
         (1..20).each do |room|
-          expect(@hotel_controller.rooms.include?(room)).must_equal true
+          expect(@hotel.rooms.include?(room)).must_equal true
         end
       end
     end
 
     describe "reserve_room" do
       it "takes two Date objects and returns a Reservation" do
-        start_date = @date
-        end_date   = start_date + 3
-        reservation = @hotel_controller.reserve_room(start_date, end_date)
-
+        reservation = @hotel.reserve_room(test_date(1), test_date(4))
         expect(reservation).must_be_kind_of Hotel::Reservation
+      end
+
+      it "adds to the list of reservations" do # ****
+        reservation1 = @hotel.reserve_room(test_date(1), test_date(4))
+        expect(@hotel.reservations.size).must_equal 1
+        reservation2 = @hotel.reserve_room(test_date(1), test_date(4))
+        expect(@hotel.reservations.size).must_equal 2
+      end
+
+      it "selects the first available room" do
+        # reserve a room, than check if the reservation room equals to 1
+        # reserve another room, than check if the reservationn room equals 2
+      end
+
+      it "returns error if no rooms are available" do
+        # reserve 20 rooms (20.times). The 21st should return an error.
+      end
+
+      it "does not accept the same date for start and end" do
+        # create a reservation from 2020/01/05 to 2020/01/05. It should raise an exception
+      end
+
+      it "does not accept a start date after the end date" do
+        # create a reservation from 2020/01/05 to 2020/01/01. It should raise an exception
       end
     end
 
   # access the list of reservations for a specific date, so that I can track reservations by date
-    describe "reservations" do
+    describe "reservations_by_date" do
       it "takes a Date and returns a list of Reservations" do
-        start_date = @date
-        end_date   = start_date + 3
-        reservation1 = @hotel_controller.reserve_room(start_date, end_date)
-        reservation2 = @hotel_controller.reserve_room(start_date, end_date)
+        reservation1 = @hotel.reserve_room(test_date(1), test_date(4))
+        reservation2 = @hotel.reserve_room(test_date(1), test_date(4))
 
-        reservations_ondate = @hotel_controller.reservations(@date)
+        reservations_ondate = @hotel.reservations_by_date(test_date(1))
 
         expect(reservations_ondate).must_be_kind_of Array
         expect(reservations_ondate.size).must_equal 2
@@ -51,26 +71,55 @@ describe "Hotel::HotelController" do
           expect(reservation).must_be_kind_of Hotel::Reservation
         end
       end
+
+      it "returns the reservation when you ask for the start date" do
+        reservation1 = @hotel.reserve_room(test_date(1), test_date(4))
+        reservations_ondate = @hotel.reservations_by_date(test_date(1))
+        expect(reservations_ondate.size).must_equal 1 # ****
+        # create another reservation from 2020/01/01 to 2020/01/05, than get a list of reservations for 2020/01/01. It should return 2
+        # create another reservation from 2020/01/05 to 2020/01/10, than get a list of reservations for 2020/01/01. It should return 2
+      end
+
+      it "returns the reservation when you ask for a date in the middle" do
+        # create a reservation from 2020/01/01 to 2020/01/10, than get a list of reservations for 2020/01/05. It should return 1
+        # create a reservation from 2020/01/02 to 2020/01/15, than get a list of reservations for 2020/01/05. It should return 2
+        # create a reservation from 2020/01/10 to 2020/01/15, than get a list of reservations for 2020/01/05. It should return 2
+      end
+
+      it "does not return a reservation when you ask for the end date" do
+        # create a reservation from 2020/01/01 to 2020/01/10, than get a list of reservations for 2020/01/10. It should return 0
+        # create a reservation from 2020/01/05 to 2020/01/10, than get a list of reservations for 2020/01/10. It should return 0
+      end
+
     end
   end
 
-  # check whether reservations conflict with each other (this will come in wave 2!)
+  # check whether reservations conflict with each other
   describe "wave 2" do
     describe "available_rooms" do
       it "takes two dates and returns a list" do
-        start_date = @date
-        end_date   = start_date + 3
-        room = 10
-        room_list = @hotel_controller.available_rooms(start_date, end_date, room)
-
+        room_list = @hotel.available_rooms(test_date(1), test_date(4))
         expect(room_list).must_be_kind_of Array
       end
+
+      it "removes a room after it is reserved" do
+        reservation1 = @hotel.reserve_room(test_date(1), test_date(5))
+        expect(@hotel.available_rooms(test_date(1), test_date(5)).size).must_equal 19
+        reservation1 = @hotel.reserve_room(test_date(1), test_date(5))
+        expect(@hotel.available_rooms(test_date(1), test_date(5)).size).must_equal 18
+      end
+
+      it "does not show the room available if there is an overlaping reservation" do
+        # create a reservation from 2020/01/05 to 2020/01/15.
+        # Check if the list of available rooms from 2020/01/10 to 2020/01/20 has 19 rooms
+        # Check if the list of available rooms from 2020/01/01 to 2020/01/10 has 19 rooms
+        # Check if the list of available rooms from 2020/01/07 to 2020/01/12 has 19 rooms
+        # Check if the list of available rooms from 2020/01/01 to 2020/01/20 has 19 rooms
+        # Check if the list of available rooms from 2020/01/01 to 2020/01/03 has 20 rooms
+        # Check if the list of available rooms from 2020/01/17 to 2020/01/20 has 20 rooms
+        # Check if the list of available rooms from 2020/01/15 to 2020/01/20 has 20 rooms
+      end
+
     end
   end
 end
-
-# make a reservation of a room for a given date range, and that room will not be part of any other reservation overlapping that date range
-# want an exception raised if I try to reserve a room during a date range when all rooms are reserved, so that I cannot make two reservations for the same room that overlap by date
-
-# Details
-# A reservation is allowed start on the same day that another reservation for the same room ends
