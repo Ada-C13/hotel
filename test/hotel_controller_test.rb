2893790bf5c8eab727744601e128a80888f4da5a
Nikki Vaughan <vnikki13@gmail.com>
2020-03-03 22:09:07 -0800

    tests for various was to request information about reservations

--- test/hotel_controller_test.rb
+++ test/hotel_controller_test.rb
@@ -1,44 +1,67 @@
 require_relative 'test_helper'
 
 describe 'HotelController class' do 
-  describe 'HotelController making reservations' do
-    before do
-      @start_date = Date.new(1993, 2, 24)
-      @end_date = Date.new(1993, 2, 26)
-      @reservation = Hotel::HotelController.reserve_room(@start_date, @end_date)
+  before do 
+    # Makes a new instance of hotel
+    @new_hotel = Hotel::HotelController.new
+
+    # Makes 20 rooms in hotel
+    20.times do |room_num|
+      room = Hotel::Room.new(room_num)
+      @new_hotel.add_room(room)
     end
 
+    # Makes reservations for hotel
+    @start_date = Date.new(1993, 2, 24)
+    @end_date = Date.new(1993, 2, 26)
+    @reservation = @new_hotel.reserve_room(@start_date, @end_date)
+  end
+
+  describe 'HotelController adding and listing rooms' do 
+    it 'should have 20 rooms' do
+      expect(@new_hotel.rooms).must_be_kind_of Array
+      expect(@new_hotel.rooms.length).must_equal 20
+    end
+
+    it 'lists all rooms in hotel' do
+      expect(@new_hotel.list_of_all_rooms).must_be_same_as @new_hotel.rooms
+    end
+  end
+
+  describe 'HotelController making reservations' do
     it 'makes a reservation with start and end dates' do
       expect(@reservation).must_be_instance_of Hotel::Reservation
     end
 
     it 'raises an Error if there are not two seperate dates given' do
-      same_date = Date.new(1993, 2, 24)
-      expect{Hotel::HotelController.reserve_room(@start_date, same_date)}.must_raise ArgumentError
+      expect{@new_hotel.reserve_room(@start_date, @start_date)}.must_raise ArgumentError
     end
 
     it 'raises an Error if the end date is before the start date' do
-      expect{Hotel::HotelController.reserve_room(@end_date, @start_date)}.must_raise ArgumentError
+      expect{@new_hotel.reserve_room(@end_date, @start_date)}.must_raise ArgumentError
     end
   end
 
   describe 'HotelController listing reservations' do
-    before do
-      start_date = Date.new(1993, 2, 24)
-      end_date = Date.new(1993, 2, 26)
-      @reservation = Hotel::HotelController.reserve_room(start_date, end_date)
+    it 'should have instances of reservation' do 
+      expect(@new_hotel.reservations).must_be_kind_of Array
+      expect(@new_hotel.reservations.first).must_be_kind_of Hotel::Reservation
     end
 
-    it 'lists all rooms in hotel' do
+    it 'selects specific reservations given a single date' do
+      date = @new_hotel.reservations.select{ |res| res.arrive == @start_date}
+      expect(@new_hotel.list_reservations(given_date: @start_date)).must_equal date
+    end
+
+    it 'lists reservations for a specific room' do
+      room_1 = @new_hotel.reservations.select{ |res| res.room_num == 1 && res.arrive == @start_date}
+      expect(@new_hotel.list_reservations(given_date: @start_date, room: 1)).must_equal room_1
 
     end
 
-    it 'lists reservations for specific room' do
-      Hotel::HotelController.list_reservations(@start_date)
-    end
-
-    it 'lists reservations for specific date range or single date given' do
-      
+    it 'lists reservations for a set of date ranges' do
+      dates = @new_hotel.reservations.select{ |res| res.arrive >= @start_date && res.depart <= @end_date }
+      expect(@new_hotel.list_reservations(given_date: @start_date, second_date: @end_date)).must_equal dates
     end
   end
 end
