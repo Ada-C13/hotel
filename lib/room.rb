bce125a23d2c163a1c7897b27198ca3fc50920a6
Jessica Stone <jstone4815@gmail.com>
2020-03-03 18:31:42 -0800

    refactor check_availability method

--- lib/room.rb
+++ lib/room.rb
@@ -9,16 +9,18 @@ module Hotel
       @cost = cost
     end
 
+    # TODO: determine why hotel_manager_test > list_reservations_by_room > returns the correct reservations is failing
     def check_availability(date_range)
       if @reservations.empty?
         return true
       else
         @reservations.each do |reservation|
-          if (reservation.range[-1] == 
-            date_range.range[0]) || (reservation.range[0] == date_range.range[-1])
-            next
-          elsif (reservation.range & date_range.range).any?
-            return false
+          if (reservation.range & date_range.range).any?
+            if (reservation.range[-1] == date_range.range[0]) || (reservation.range[0] == date_range.range[-1])
+              next
+            else
+              return false
+            end
           end
         end
         return true
