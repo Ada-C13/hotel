1f9b4976800f5d1bd52217d19db9467a582f2b57
Jessica Liang <jessicursliang@gmail.com>
2020-03-04 15:46:19 -0800

    adding back in classes for Room and DateRange

---
+++ lib/date_range.rb
@@ -0,0 +1,32 @@
+module Hotel
+
+	class DateRange
+
+		attr_reader :start_date, :end_date
+
+		def initialize(start_date, end_date)
+			@start_date = start_date
+			@end_date = end_date
+		end
+
+		# Checks if a range falls within another range.
+		def include_range?(token, incoming_range)
+			if token == :booking 
+				return (incoming_range.start_date >= self.start_date) && (incoming_range.end_date < self.end_date)
+			elsif token == :searching 
+				return (incoming_range.start_date >= self.start_date) && (incoming_range.end_date <= self.end_date)
+			else
+				return false
+			end
+		end
+
+		# Checks if a range includes a single date.
+		def include_single_date?(incoming)
+			range = @start_date...@end_date
+
+			range.include?(incoming) ? true : false
+		end
+
+	end
+
+end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -2,36 +2,44 @@ require_relative "test_helper"
 
 describe Hotel::Reservation do
 
-	describe "Wave 1 Requirements" do
-
+	describe "#initialize" do
 		before do
-			room = 20
+			room = Hotel::Room.new(20)
 			start_date = Date.new(2019, 12, 23)
 			end_date = Date.new(2019, 12, 28)
 
-			@new_reservation = Hotel::Reservation.new(room, start_date, end_date)
+			@new_reservation = Hotel::Reservation.new(room, Hotel::DateRange.new(start_date, end_date))
 		end
 
-		it "makes an instance of a Reservation" do
+		it "creates an instance of Reservation" do
 			expect(@new_reservation).must_be_instance_of Hotel::Reservation
-			expect(@new_reservation.room).must_be :>, 0
-			expect(@new_reservation.room).must_be :<, 21
-			expect(@new_reservation.room).must_be_instance_of Integer
-			expect(@new_reservation.start_date).must_be_instance_of Date
-			expect(@new_reservation.end_date).must_be_instance_of Date
+		end
+
+		it "contains an instance of Room assigned" do
+			expect(@new_reservation.room).must_be_instance_of Hotel::Room
+		end
+
+		it "@length_of_stay is 5" do
 			expect(@new_reservation.length_of_stay).must_equal 5
 		end
 
-		it "returns the cost of the reservation" do
+		it "@cost will be 1000" do
 			expect(@new_reservation.cost).must_equal 1000
 		end
 
-		it "raises ArgumentError if invalid dates are provided for creating reservation" do
-			inval_end = Date.new(2001, 02, 25)
-			inval_start = Date.new(2001, 02, 26)
-
-			expect{Hotel::Reservation.new(10, inval_start, inval_end)}.must_raise ArgumentError
+		it "contains a Range assigned" do
+			expect(@new_reservation.range).must_be_instance_of Hotel::DateRange
 		end
+
+		it "raises ArgumentError if invalid dates are provided for creating reservation" do
+			inval_start = Date.new(2001, 02, 26)
+			inval_end = Date.new(2001, 02, 25)
+			
+			inval_range = Hotel::DateRange.new(inval_start, inval_end)
+
+			expect{Hotel::Reservation.new(10, inval_range)}.must_raise ArgumentError
+		end
+
 	end
 
 end
