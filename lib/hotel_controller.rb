3fd66305133fdae4d8f8f67f6b18723b74736548
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-03 22:06:32 -0800

    Wave 1 find_by_date method added, and test

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -9,24 +9,30 @@ module Hotel
     attr_reader :rooms
     # hotel_controller needs to have a list of all its reservations,
     # initialized with nil to begin
-    def initialize(all_reservations: nil)
-      @all_reservations = all_reservations || []
+    def initialize
       create_rooms
+      @all_reservations = []
     end
 
     # Wave 1, US 2: "I can reserve a room for a given date range, so that I can make a reservation"
-    def reserve_with_range(date_range)
-      # preliminary fulfillment of basic user story with no conflict checking or available room searching
+    def reserve_with_range(input_range)
+      # preliminary fulfillment of basic functionality with no conflict-checking or available room searching
       available_room_id = 5
-      new_reservation = Hotel::Reservation.new(date_range, 5)
+      new_reservation = Hotel::Reservation.new(input_range, available_room_id)
       @all_reservations << new_reservation
       return new_reservation
     end
 
     # Wave 1, US 3: "I can access the list of reservations for a specific date, so that I can track reservations by date"
-    # def find_by_date(date_range)
-
-    # end
+    def find_by_date(input_range)
+      reservations_by_date = []
+      @all_reservations.each do |reservation|
+        if input_range == reservation.date_range
+          reservations_by_date << reservation
+        end
+      end
+      return reservations_by_date
+    end
 
 
     private
