258a8d1cecac64319a7cbda7535e7d642c997b3e
denisseai <msdenisseanaya@gmail.com>
2020-03-03 19:07:20 -0800

    Added StayRange info to Reservation with tests

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,10 +1,13 @@
 
 module Hotel
   class Reservation
-    attr_reader :id
+    attr_reader :id, :room, :start_date, :end_date
 
     def initialize(id:, room:, start_date:, end_date:)
       @id = id
+      @room = room
+      @start_date = Date.parse(start_date)
+      @end_date = Date.parse(end_date)
     end
   end
   
