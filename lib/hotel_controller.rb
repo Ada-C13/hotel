1f923ab00a13d6bbfc2a77189f52699a065e248e
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-03-04 16:09:01 -0800

    Wrote DateRange's overlap? method and psuedocode for HotelController's find available room

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -11,7 +11,9 @@ module Hotel
 
     def make_reservation(check_in:, check_out:)
       date_range = DateRange.new(check_in: check_in, check_out: check_out)
-      room = @rooms.sample
+      # if reservations.length > 0, room = find_available_room(date_range), 
+      # else room = @rooms[0] for first reservation
+      room = @rooms.sample # 
       reservation = Reservation.new(date_range: date_range, room: room)
       @reservations << reservation
       return reservation
@@ -25,9 +27,13 @@ module Hotel
       # call include? ?
     # end
 
-    # def find_available_rooms
-      # call overlap? ?
-    # end
+    def find_available_room(new_date_range)
+      # available_rooms = @rooms.clone
+      # pass in new DateRange object, which has a start_date and end_date to compare against... 
+      # @reservations.each do reservation.date_range.overlap?(new_date_range)
+      # if overlap? returns true, available_rooms.delete(reservation.room)
+      # return available_rooms.sample
+    end
 
     # def show_all_rooms
       # @rooms.each |room| do puts "room: " room end
