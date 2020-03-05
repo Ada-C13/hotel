ad7a085b206945cc57038aacf6178b28c859f830
Olga Patrakova <olgapatrakova@outlook.com>
2020-03-04 21:26:11 -0800

    #overlap? method for DateRange

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,5 +1,3 @@
-require_relative 'room'
-
 module Hotel
   class DateRange
     attr_reader :start_date, :end_date
@@ -11,5 +9,11 @@ module Hotel
       @start_date = start_date
       @end_date = end_date
     end
+
+    def overlap?(range)
+      self.start_date < range.end_date && self.end_date > range.start_date 
+    end
   end
 end
+
+
--- test/ranges_test.rb
+++ test/ranges_test.rb
@@ -10,4 +10,38 @@ describe "Date Range" do
       expect{Hotel::DateRange.new(start_date: "7 Mar 2020", end_date: "5 Mar 2020")}.must_raise ArgumentError
     end
   end
+
+  describe "#overlap?" do
+    it "checks 2 ranges for overlapping" do
+      range1 = Hotel::DateRange.new(start_date: "3 Mar", end_date: "5 Mar")
+      range2 = Hotel::DateRange.new(start_date: "4 Mar", end_date: "6 Mar")
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "checks 2 ranges for overlapping" do
+      range1 = Hotel::DateRange.new(start_date: "1 Mar", end_date: "10 Mar")
+      range2 = Hotel::DateRange.new(start_date: "7 Mar", end_date: "8 Mar")
+      expect(range1.overlap?(range2)).must_equal true
+
+      range1 = Hotel::DateRange.new(start_date: "5 Mar", end_date: "6 Mar")
+      range2 = Hotel::DateRange.new(start_date: "1 Mar", end_date: "10 Mar")
+      expect(range1.overlap?(range2)).must_equal true
+
+      range1 = Hotel::DateRange.new(start_date: "5 Mar", end_date: "15 Mar")
+      range2 = Hotel::DateRange.new(start_date: "1 Mar", end_date: "10 Mar")
+      expect(range1.overlap?(range2)).must_equal true
+    end
+
+    it "returns false if check-in date is the same as another check-out date" do
+      range1 = Hotel::DateRange.new(start_date: "3 Mar", end_date: "5 Mar")
+      range2 = Hotel::DateRange.new(start_date: "5 Mar", end_date: "7 Mar")
+      expect(range1.overlap?(range2)).must_equal false
+    end
+
+    it "returns false if there is no overlapping" do
+      range1 = Hotel::DateRange.new(start_date: "3 Mar", end_date: "5 Mar")
+      range2 = Hotel::DateRange.new(start_date: "6 Mar", end_date: "8 Mar")
+      expect(range1.overlap?(range2)).must_equal false
+    end
+  end
 end
