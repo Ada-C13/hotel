5434d20484c40ebd2109786bfcb6aff964aae122
Hannah <hannahjoo24@gmail.com>
2020-03-04 20:58:25 -0800

    Modified room.rb and test file

--- lib/room.rb
+++ lib/room.rb
@@ -1,18 +1,15 @@
 module Hotel 
   class Room
-    attr_reader :number, :vacancy
+
+    NUMBERS = (1..20)
+
+    attr_reader :number
 
     def initialize(number)
-      if (number.class != Integer) || (number <= 0) || (number >= 21)
-        # con
+      if (number.class != Integer) || !NUMBERS.include?(number)
         raise ArgumentError, "#{number} is an invalid value"
       end 
       @number = number 
-      @vacancy = true
-    end 
-
-    def change_vacancy
-      @vacancy = false if @vacancy == true
     end 
   end 
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -7,66 +7,26 @@ describe "Room class" do
   end 
 
   describe "#initialize" do 
-    it "Creates number and vacancy" do 
+
+    it "creates number" do 
       expect(@room).must_respond_to :number
-      expect(@room).must_respond_to :vacancy
-
       expect(@room.number).must_be_instance_of Integer
-      expect(@room.vacancy).must_be_instance_of TrueClass 
-
       expect(@room.number).must_equal 5 
-      expect(@room.vacancy).must_equal true
     end 
 
-    it "Rasise an ArgumentError if number is not between 1 and 20" do 
-      expect{
-        Hotel::Room.new(21)
-      }.must_raise ArgumentError
-
-      expect{
-        Hotel::Room.new(0)
-      }.must_raise ArgumentError
-
-      expect{
-        Hotel::Room.new(100)
-      }.must_raise ArgumentError
-      
+    it "rasise an ArgumentError if number is not between 1 and 20" do 
+      expect{Hotel::Room.new(21)}.must_raise ArgumentError
+      expect{Hotel::Room.new(0)}.must_raise ArgumentError
+      expect{Hotel::Room.new(100)}.must_raise ArgumentError
     end 
 
-    it "Raises an ArgumentError if number is not an Integer" do 
+    it "raises an ArgumentError if number is not an Integer" do 
 
-      # Act & Assert
-      expect{
-        Hotel::Room.new("string")
-      }.must_raise ArgumentError
-
-      expect{
-        Hotel::Room.new([])
-      }.must_raise ArgumentError
-
-      expect{
-        Hotel::Room.new("123")
-      }.must_raise ArgumentError
+      expect{Hotel::Room.new("string")}.must_raise ArgumentError
+      expect{Hotel::Room.new([])}.must_raise ArgumentError
+      expect{Hotel::Room.new("123")}.must_raise ArgumentError
     end 
   end  
 
-
-  describe "#change_vacancy" do
-    it "Changes vacancy as unavailable" do
-      # Act 
-      @room.change_vacancy
-
-      # Assert 
-      expect(@room.vacancy).must_equal false
-    end 
-
-    it "Does not vacancy as unavailable" do
-      # Arrange
-      room = Hotel::Room.new(3)
-
-      # Act & Assert 
-      expect(room.vacancy).must_equal true
-    end 
-  end 
 end 
 
