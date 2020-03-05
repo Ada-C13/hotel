ab01deb63b203dcf30f47872238e121e057023dc
RebeccaRoach <rebeccaroach12@gmail.com>
2020-03-04 16:35:47 -0800

    Defined overlap? method on DateRange class, tests passing.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -4,6 +4,7 @@ module Hotel
   class DateRange
 
     # start_date and #end_date are required to be date objects
+    attr_reader :start_date, :end_date
     def initialize(start_date, end_date)
       @start_date = start_date
       @end_date = end_date
@@ -13,10 +14,17 @@ module Hotel
     end
 
     def overlap?(comparison_range)
-      # if comparison_range.start_date 
-      #   # is at least one of the dates inside start or end?
-
-      #   || comparison_range.end_date 
+      if comparison_range.start_date >= self.start_date && comparison_range.start_date < self.end_date
+        return true
+      elsif comparison_range.end_date <= self.end_date && comparison_range.end_date > self.start_date
+        return true
+      # elsif comparison_range.start_date >= self.start_date && comparison_range.end_date < self.end_date
+        return true
+      elsif comparison_range.start_date <= self.start_date && comparison_range.end_date >= self.end_date
+        return true
+      else
+        return false
+      end
     end
 
   end
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -17,6 +17,7 @@ describe Hotel::DateRange do
         Hotel::DateRange.new(@end_date, @start_date)
       end.must_raise ArgumentError
     end
+  end
 
     # come back to clean this up using let syntax later
     # it "throws an argument error if inputs are not Date objects" do
@@ -27,5 +28,55 @@ describe Hotel::DateRange do
     #   end.must_raise ArgumentError
     # end
 
-  end
+
+    describe "overlap?" do
+      before do
+        # 1 - 2 - 3
+        #     2 - 3
+        # 1 - 2
+        @start_date = Date.new(2020, 1, 1)
+        @end_date = Date.new(2020, 1, 3)
+        @date_range_1 = Hotel::DateRange.new(@start_date, @end_date)
+        @comparison_range_1 = Hotel::DateRange.new(@start_date+1, @end_date)
+      end
+
+      it "returns true if the comparison range start date is inside of current range" do
+        puts @date_range_1.start_date
+        expect(@date_range_1.overlap?(@comparison_range_1)).must_equal true
+      end
+
+      it "returns true if the comparison range end date is inside of current range" do
+        comparison_range_2 = Hotel::DateRange.new(@start_date, @end_date-1)
+        expect(@date_range_1.overlap?(comparison_range_2)).must_equal true
+      end
+
+      it "returns true if the comparison range encompasses or equals all of the current range" do
+        comparison_range_3 = Hotel::DateRange.new(@start_date-1, @end_date+1)
+        expect(@date_range_1.overlap?(comparison_range_3)).must_equal true
+
+        comparison_range_4 = Hotel::DateRange.new(@start_date, @end_date)
+        expect(@date_range_1.overlap?(comparison_range_4)).must_equal true
+
+        comparison_range_5 = Hotel::DateRange.new(@start_date, @end_date+1)
+        expect(@date_range_1.overlap?(comparison_range_5)).must_equal true
+
+        comparison_range_6 = Hotel::DateRange.new(@start_date-1, @end_date)
+        expect(@date_range_1.overlap?(comparison_range_6)).must_equal true
+      end
+
+      it "returns false if the comparison range does not overlap with the current range" do
+        comparison_range_7 = Hotel::DateRange.new(@start_date-3, @end_date-3)
+        expect(@date_range_1.overlap?(comparison_range_7)).must_equal false
+      
+        comparison_range_8 = Hotel::DateRange.new(@start_date+3, @end_date+3)
+        expect(@date_range_1.overlap?(comparison_range_8)).must_equal false
+      end
+
+      it "returns false if the comparison range starts on the current end date" do
+        comparison_range_9 = Hotel::DateRange.new(@end_date, @end_date+1)
+        expect(@date_range_1.overlap?(comparison_range_9)).must_equal false
+      end
+    end
+
+    
 end
