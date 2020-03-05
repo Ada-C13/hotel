aab4b43b356d3e2baf9fd203d2dd329e42b10d8d
Hannah <hannahjoo24@gmail.com>
2020-03-04 21:00:43 -0800

    Modified hotel_manager.rb and test file

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -4,44 +4,98 @@ require_relative 'room'
 
 module Hotel 
   class HotelManager 
+
+    ROOM_NUMBERS = 20 
+
     attr_reader :rooms, :reservations 
 
-    def initialize(rooms = [])
-      20.times do |i| 
-        rooms << Hotel::Room.new(i + 1)
+    def initialize
+      @rooms = []
+      @reservations = []
+
+      ROOM_NUMBERS.times do |i| 
+        @rooms << Hotel::Room.new(i + 1)
+      end 
+    end 
+
+    # (1)list of reservations with a given date range & room => returning reservation
+    #  overlap (booking)
+    def reservations_per_room(date_range, room)
+      return @reservations.select do |reservation| 
+        # If the room number is the same
+        # check the reservation inside of the room 
+        (reservation.room.number == room.number) && (reservation.date_range.overlap?(date_range))
+      end 
+    end 
+
+    
+    # (2) Find available rooms (with given date range) 
+    #  and use the "reservations_per_room" from (1)
+    def available_room_numbers(date_range) 
+ 
+      # Find available rooms that are not reserved for the given date range
+      available_rooms = @rooms.select do |room| 
+        reservations = reservations_per_room(date_range, room)
+        reservations.empty?
       end 
 
-      @rooms = rooms
-      @reservations = []
+      # Return room numbers
+      return available_rooms.map { |room| room.number }
     end 
 
 
-    def available_room(date_range)
-      date_range.valid_dates?
+    # (3) make reservation
+    # if we have available rooms, create a new instance of Reservation
+    # if not, room is not available 
+    def make_reservation(date_range)
 
-      return @rooms.find { |room| room.vacancy == true }
-    end
+      # Find available rooms for the specific date range
+      available_room_numbers = available_room_numbers(date_range) 
+
+      # If available_rooms exist
+      if available_room_numbers.length > 0 
+
+        # Create an instance of Reservation
+        room = find_room_by_number(available_room_numbers[0])
+        reservation = Reservation.new(date_range, room)
+        
+        self.add_reservation(reservation)
+
+        return reservation
+      end 
+  
+      raise ArgumentError, "There are no available rooms"
+    end 
 
 
-    def make_reservation(date_range) #Date.new(start_date, end_date)
-      date_range.valid_dates?
+    # (4) Find reservations for date => include (searching)
+    # iterate through reservations
+    def find_all_reservations(date)
 
-      room = self.available_room(date_range)
-      reservation = Hotel::Reservation.new(date_range, room)
+      list = @reservations.filter do |reservation|
+        # modified
+        reservation.date_range.include?(date) #end_date should be excluded
+      end 
 
-      # Invoke the method
-      reservation.room.change_vacancy
+      # TO DO
+      # Make a custom error
+      if list.empty? 
+        raise ArgumentError, "No reservations found"
+      end 
 
-      # Add reservation
+      return list
+    end 
+
+
+    # Helper method
+    def add_reservation(reservation)
       @reservations << reservation
-
-      return reservation
-    end 
-
-
-    def find_reservations_by_date(date_range)
-      return @reservations.filter { |reservation|
-        reservation.date_range == date_range } 
+    end
+    
+    # TO DO
+    # add test
+    def find_room_by_number(number)
+      return @rooms.find { |room| room.number == number}
     end 
   end 
 end 
--- test/hotel_manager_test.rb
+++ test/hotel_manager_test.rb
@@ -1,93 +1,158 @@
-require_relative 'test_helper'
+require_relative "test_helper"
 
-describe "HotelManager class" do 
+describe "HotelManager class" do
+  before do
+    @hotel_manager = Hotel::HotelManager.new()
+    @date = Date.new(2020, 8, 10)
 
-  # Arrange 
-  before do 
-     @hotel_manager = Hotel::HotelManager.new()
+    @date_range = Hotel::DateRange.new(Date.new(2020, 8, 10), Date.new(2020, 8, 15))
+  end
 
-     @date_range = Hotel::DateRange.new("2020-3-20", "2020-3-27")
-     @date_range_2 =  Hotel::DateRange.new("2020-3-5", "2020-3-10")
+  # wave 1
+  describe "wave 1" do
+    describe "#initialize" do
+      it "returns a list of rooms" do
+        rooms = @hotel_manager.rooms
+        expect(rooms).must_be_kind_of Array
 
-    # 1st reservation
-    @hotel_manager.make_reservation(@date_range)
+        rooms.each do |room|
+          expect(room).must_be_instance_of Hotel::Room
+        end 
 
-    # 2nd reservation
-    @hotel_manager.make_reservation(@date_range_2)
-  end 
+        expect(rooms[0].number).must_equal 1
+      end
+    end
 
+    describe "#reservations_per_room" do 
+      it "returns a list of reservations for the specific room" do 
 
-  describe "#initialize" do 
-    # I can access the list of all of the rooms in the hotel
-    it "Creates rooms" do
-      # Assert
-      expect(@hotel_manager).must_respond_to :rooms
+        room = @hotel_manager.rooms[0]
 
-      expect(@hotel_manager.rooms).must_be_instance_of Array
+        reservations = @hotel_manager.reservations_per_room(@date_range, room)
 
-      @hotel_manager.rooms.each do |room|
-        expect(room).must_be_instance_of Hotel::Room
-      end  
-    end 
+        expect(reservations).must_be_kind_of Array
 
-    # I access the list of reservations for a specified room and a given date range
-    it "Creates reservations" do 
-  
-      expect(@hotel_manager).must_respond_to :reservations
-
-      expect(@hotel_manager.reservations).must_be_instance_of Array   
-      
-      @hotel_manager.reservations.each do |reservation|
-        expect(reservation).must_be_instance_of Hotel::Reservation
+        reservations.each do |reservation| 
+          expect(reservation).must_be_kind_of Hotel::Reservation
+        end 
       end 
     end 
 
-    it "Returns the first reservation" do 
-      expect(@hotel_manager.reservations[0].room.number).must_equal 1
-      expect(@hotel_manager.reservations[0].date_range).must_be_instance_of Hotel::DateRange
+    # TO DO (think if there are more tests to add)
+    describe "#make_reservation" do 
+      it "takes a date range and returns a Reservation" do 
+        reservation = @hotel_manager.make_reservation(@date_range)
+
+        expect(reservation).must_be_instance_of Hotel::Reservation 
+        expect(reservation.room).must_be_instance_of Hotel::Room
+        expect(reservation.room.number).must_equal 1
+      end 
+
+      it "raise an Arugment error after making 20 reservations for the same date range" do 
+
+        20.times do 
+          @hotel_manager.make_reservation(@date_range)
+        end 
+ 
+        expect{
+          @hotel_manager.make_reservation(@date_range)
+        }.must_raise ArgumentError
+      end 
     end 
 
-    it "Returns the last reservation" do 
-      last_index = @hotel_manager.reservations.length - 1
-      expect(@hotel_manager.reservations[last_index].room.number).must_equal last_index + 1
+
+    describe "#find_all_reservations" do 
+
+      it "raises an ArgumentError when there is no reservation" do 
+        expect{@hotel_manager.find_all_reservations(@date)}.must_raise ArgumentError
+      end 
+
+
+      it "returns 5 reservations on the specific date" do 
+        5.times do 
+          @hotel_manager.make_reservation(@date_range)
+        end 
+
+        reservations = @hotel_manager.find_all_reservations(@date)
+
+        expect(reservations).must_be_kind_of Array
+        expect(reservations.length).must_equal 5
+        expect(reservations[0]).must_be_kind_of Hotel::Reservation
+      end 
+
+      it "returns 5 reservations on the specific date" do 
+
+        @hotel_manager.make_reservation(@date_range)
+
+        # TO DO
+        # no overlap
+        test_date_range = Hotel::DateRange.new(Date.new(2020, 07, 11), Date.new(2020, 07, 13))
+
+        10.times do 
+          @hotel_manager.make_reservation(test_date_range)
+        end 
+
+        # overlap
+        test_date_range_2 = Hotel::DateRange.new(Date.new(2020, 07, 6), Date.new(2020, 07, 12))
+
+        9.times do 
+          @hotel_manager.make_reservation(test_date_range_2)
+        end 
+
+
+        test_date = Date.new(2020, 07, 11)
+        reservations = @hotel_manager.find_all_reservations(test_date)
+
+        expect(reservations).must_be_kind_of Array
+        expect(reservations.length).must_equal 19
+        expect(reservations.last.room.number).must_equal 19
+      end 
     end 
-  end 
 
-  describe "#available_room" do 
-    it "Returns an available room" do 
-      # Arrange 
-      date_range_3 = Hotel::DateRange.new("2020-3-5", "2020-3-10")
+    describe "#add_reservation" do 
+      it "adds a reservation in reservations" do 
+        room = Hotel::Room.new(8)
+        reservation = Hotel::Reservation.new(@date_range, room)
 
-      # 3rd reservation
-      @hotel_manager.make_reservation(date_range_3)
+        reservations = @hotel_manager.add_reservation(reservation)
 
-      # Act & Assert
-      expect(@hotel_manager.available_room(date_range_3).number).must_equal 4
+        expect(reservations).must_be_instance_of Array
+        expect(reservations.length).must_equal 1
+        expect(reservations[0].room.number).must_equal 8
+      end 
     end 
-  end 
+  end
 
+  describe "wave 2" do 
+    describe "#available_room_numbers" do 
+      it "returns available rooms for the given date range" do 
 
-  describe "#make_reservation" do 
-    it "Makes a reservation within valid date range" do 
-      # Assert 
-      expect(@hotel_manager).must_respond_to :make_reservation
+        available_room_numbers = @hotel_manager.available_room_numbers(@date_range)
 
-      expect(@hotel_manager.make_reservation(@date_range)).must_be_instance_of Hotel::Reservation
-    end 
-  end 
+        expect(available_room_numbers).must_be_kind_of Array
+        expect(available_room_numbers.length).must_equal 20 
 
+        expect(available_room_numbers[0]).must_be_kind_of Integer
+      end 
 
-  # I can access the list of reservations for a specific date, so that I can track reservations by date
-  describe "#find_reservations_by_date" do 
-    
-    it "Finds the list of reservations for a specific date" do 
-      reservations_list = @hotel_manager.find_reservations_by_date(@date_range)
+      it "returns the room #1 after making 20 reservations in a different date range" do 
+        date_range = Hotel::DateRange.new(Date.new(2020, 8, 10), Date.new(2020, 8, 15))
+        new_date_range = Hotel::DateRange.new(Date.new(2020, 8, 1), Date.new(2020, 8, 6))
 
-     # Assert
-      expect(reservations_list).must_be_instance_of Array 
-      reservations_list.each do |reservation|
-        expect(reservation).must_be_instance_of Hotel::Reservation
+        20.times do 
+          @hotel_manager.make_reservation(date_range)
+        end 
+
+        expect(@hotel_manager.available_room_numbers(new_date_range)[0]).must_equal 1
+      end 
+
+      it "returns the room #20 after making 19 reservations for the same date range" do 
+        19.times do 
+          @hotel_manager.make_reservation(@date_range)
+        end 
+ 
+        expect(@hotel_manager.available_room_numbers(@date_range)[0]).must_equal 20
       end 
     end 
   end 
-end 
+end
