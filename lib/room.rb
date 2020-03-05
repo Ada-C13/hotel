b70128d770c80daf1255422308b5f1ce126d725c
Angela Nguyen <angethuy@gmail.com>
2020-03-04 17:26:24 -0800

    rooms no longer track reservations

--- lib/room.rb
+++ lib/room.rb
@@ -2,17 +2,13 @@ require_relative 'reservation'
 
 module Hotel
   class Room
-    attr_reader :id, :reservations, :price, :type
+    attr_reader :id, :price, :type
 
     def initialize(id)
       @id = id.to_s
-      @reservations = []
       @price = 200
       @type = "standard"
     end
-
-    # TO-DO: add inspect to display all reservations
-
   end
 
 end
