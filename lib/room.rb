cdaefad75ade034cdbfff1c8ebbc5f4c0e394077
Nikki Vaughan <vnikki13@gmail.com>
2020-03-04 21:39:34 -0800

    added a reservation attribute and revised

--- lib/room.rb
+++ lib/room.rb
@@ -1,13 +1,13 @@
 require_relative 'hotel_controller'
 
 module Hotel
-  attr_reader :room_num, :rate
-
   class Room
+    attr_reader :room_num, :rate, :reservations
+    
     def initialize(room_num)
       @room_num = room_num
       @rate = 200
+      @reservations = []
     end
-
   end
 end
