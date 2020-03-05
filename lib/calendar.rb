6973a01f146f59328b37358595fe489b0269f3f8
mulhoo <oliviamulholland94@gmail.com>
2020-03-05 11:35:01 -0800

    updated calendar code, extends to all dates in res

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -11,15 +11,28 @@ class Calendar
   def add_to_calendar(reservation)
   
     if !calendar.key?(reservation[:start_date])
-      calendar[reservation[:start_date]] = []
-      calendar[reservation[:start_date]] << reservation
-    elsif @calendar.key?(reservation[:start_date])
-      if calendar[reservation[:start_date]].length == 20
-        raise NoRoomError.new ("There are no available rooms for the duration of specified dates.")
+      reservation[:total_nights].times do |i|
+        i += 0 
+        calendar[(Date.parse(reservation[:start_date]) + i).iso8601] = []
+        calendar[(Date.parse(reservation[:start_date]) + i).iso8601] << reservation 
       end
-      calendar[reservation[:start_date]] << reservation
-    end
   
-  end
+    elsif calendar.key?(reservation[:start_date])
+      reservation[:total_nights].times do |i|
+        i += 0 
 
+        if !calendar.key?((Date.parse(reservation[:start_date]) + i).iso8601)
+          calendar[(Date.parse(reservation[:start_date]) + i).iso8601]= []
+          calendar[(Date.parse(reservation[:start_date]) + i).iso8601] << reservation 
+
+        else 
+          if reservation[:start_date].length == 20
+            raise NoRoomError.new ("There are no available rooms for the duration of specified dates.")
+          end
+          calendar[(Date.parse(reservation[:start_date]) + i).iso8601] << reservation 
+        end
+
+      end
+    end
+  end
 end
