3247f981e7925e6013ffc7f9bf6bf339a332802f
dnguye2 <dnguyen1093@gmail.com>
2020-03-04 19:36:19 -0800

    added tests for valid dates for reservation class

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -2,17 +2,42 @@ require_relative "test_helper"
 
 describe Hotel::Reservation do
   describe "constructor" do
+    before do
+      @reservation = Hotel::Reservation.new("20010203","20010206", 1)
+    end
     it "is an instance of reservation" do
-      expect(Hotel::Reservation.new("2016, 12, 31","2017, 01, 01", 1)).must_be_kind_of Hotel::Reservation
+      expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+
+    it "raises an argument error for negative-length ranges" do
+      start_date = Date.new(2017, 01, 06)
+      end_date = Date.new(2017, 01, 05)
+      expect{Hotel::Reservation.new(start_date, end_date, 1)}.must_raise ArgumentError
+    end
+
+    it "raises an error when there is a 0-length range" do
+      start_date = Date.new(2017, 01, 01)
+      end_date = Date.new(2017, 01, 01)
+      
+      expect{Hotel::Reservation.new(start_date, end_date, 1)}.must_raise ArgumentError
     end
   end
 
   describe "cost" do
-    it "returns a number" do
+    before do 
       start_date = Date.new(2017, 01, 01)
       end_date = start_date + 3
-      reservation = Hotel::Reservation.new(start_date, end_date, nil)
-      expect(reservation.cost).must_be_kind_of Numeric
+      @reservation = Hotel::Reservation.new(start_date, end_date, nil)
+    end
+
+    it "returns a number" do
+      expect(@reservation.cost).must_be_kind_of Numeric
+    end
+
+    it "returns the correct cost" do
+      expect(@reservation.cost).must_equal 400
     end
   end
+
+ 
 end
