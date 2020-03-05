2aada82d72017f3bfbcf723f47d8edb321e80595
dnguye2 <dnguyen1093@gmail.com>
2020-03-04 18:53:16 -0800

    added method to parse end and start dates

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -3,15 +3,9 @@ module Hotel
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
-
       # allowing for string inputs for dates
-      if start_date.is_a?(String) && end_date.is_a?(String)
-        start_date = Date.parse(start_date)
-        end_date = Date.parse(end_date)
-      else
-        start_date = start_date
-        end_date = end_date
-      end
+      start_date = parse_start_date(start_date)
+      end_date = parse_end_date(end_date)
 
       # verifying proper date range
       difference = end_date - start_date
@@ -28,28 +22,22 @@ module Hotel
       @end_date = end_date
     end
 
-    # # maybe put this code above into verification method?
-    #  # allowing for string inputs for dates
-    #  if start_date.is_a?(String) && end_date.is_a?(String)
-    #   start_date = Date.parse(start_date)
-    #   end_date = Date.parse(end_date)
-    # else
-    #   start_date = start_date
-    #   end_date = end_date
-    # end
+    # methods to parse date
+    def parse_start_date(start_date)
+      if start_date.is_a?(String)
+        return start_date = Date.parse(start_date)
+      else
+        return start_date = start_date
+      end
+    end
 
-    # verifying proper date range
-    # difference = end_date - start_date
-    # if difference == 0
-    #   raise ArgumentError, "Cannot have 0 length date range"
-    # elsif difference < 0
-    #   raise ArgumentError, "Cannot have negative length for a date range"
-    # else
-    #   @start_date = start_date
-    #   @end_date = end_date
-    # end
-    # def verification()
-    # end
+    def parse_end_date(end_date)
+      if end_date.is_a?(String)
+        return end_date = Date.parse(end_date)
+      else
+        return end_date = end_date
+      end
+    end
 
     def overlap?(range)
 
