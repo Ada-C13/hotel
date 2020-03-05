f14c6d8265dcb55f29ebd15ee1adea29634a6c89
Vera <lizeth.veraro@outlook.com>
2020-03-04 18:14:58 -0800

    Self private validate date, to validate the format date,start date not greater than end_date and the length not equals to 0 updating the overlap? and nights method

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,28 +5,46 @@ module Hotel
     attr_accessor :start_date, :end_date
 
     def initialize(start_date, end_date)
-      self.class.validate_date(start_date)
-      self.class.validate_date(end_date)
+      self.class.validate_date?(start_date,end_date)
+      @start_date = start_date
+      @end_date = end_date
     end
 
-    def overlap?(data_range)
-      return false
+    def overlap?(input_range)
+
+      a = input_range.start_date >= self.start_date
+      b = input_range.start_date < self.end_date
+      c = input_range.end_date > self.start_date 
+      d = input_range.end_date < self.end_date
+       
+      if a && b || c && d
+        return true
+      else
+        return false
+      end
     end
 
-    def include?(date)
-      return false
+    def include?(input_range)
+  
     end
 
     def nights
-      return 3
+      nights_number = (end_date - start_date).to_i
+      return nights_number
     end
-
 
     private
-    
-    def self.validate_date(date)
-      return true
-    end
 
-  end
+    def self.validate_date?(start_date, end_date)
+
+      date_format = '%Y-%m-%d'
+      if !DateTime.strptime(start_date.to_s, date_format) || !DateTime.strptime(end_date.to_s, date_format)
+         return rescue ArgumentError
+      elsif start_date > end_date || start_date - end_date == 0
+        raise ArgumentError.new("The end time is before the start time.")
+      end
+    end
+  end 
 end
+# a = Hotel::DateRange.new("2015-10-10","2015-10-10")
+# p a
