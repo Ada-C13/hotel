5e6a73c903355b74ed5401f9cb2b0418c26da739
dnguye2 <dnguyen1093@gmail.com>
2020-03-05 18:15:06 -0800

    pseudocode for hotelcontroller methods

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -15,6 +15,9 @@ module Hotel
       @rooms = []
       @rooms << room_base.each_with_object({}) do
           |key, h| h[key] = {:date => [], :reservation => []} 
+          # may need to modify what's in my hash
+          # on the right track but need to modify
+          # just need reservation objects?
       end
 
       return @rooms
@@ -22,10 +25,26 @@ module Hotel
 
     def reserve_room(start_date, end_date)
       # start_date and end_date should be instances of class Date
+        # covered by having Reservation inherit from DateRange
+      # need to keep in mind implementation of room choice
+
+      # room chosen is based on the method! not by the user! (see README.md)
+      # if nil, default will then redirect to be first available room
+      # add reservation instance to @rooms array based off of room attribute
+      # add date instance to @rooms array
       return Reservation.new(start_date, end_date, nil)
     end
 
     def reservations(date)
+      # Purpose: takes a Date and returns a list of Reservations
+      
+      # input is date that is parsed
+      # loop through @rooms hash
+        # for each 
+       # using .between?
+          # date.between?(start_date, end_date)
+            # returng reservation.inspect
+      # .find/.detect to search through each array of @rooms and return the reservation
       return []
     end
 
