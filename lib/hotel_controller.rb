71b8182ce825318137705565401331fde07381a6
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-05 17:26:21 -0800

    finished reserve_room methods, created tests, and passed all the tests

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -8,17 +8,64 @@ module Hotel
     end
   
     # Wave 1
-    # def rooms
-    #     @hotel_controller = Hotel::HotelController.new
-    #     room_list = @hotel_controller.rooms 
-    #   return room_list
-    # end
 
-    # def reserve_room(start_date, end_date)
-    #   # start_date and end_date should be instances of class Date
-    #   return Reservation.new(start_date, end_date, nil)
-    # end
+    # Access the list of all of the rooms in the hotel
+    def self.rooms
+        room_list = HotelController.rooms 
+      return room_list
+    end
 
+    # Access the list of reservations for a specified room and a given date range
+    def reservations_list_room_and_date(given_room, given_date_range)
+      reservations_list_of_given_room_date_range = reservations.select do |res|
+        res.date_range.overlap?(given_date_range) && res.room == given_room 
+      end
+      return reservations_list_of_given_room_date_range
+    end
+
+   # Add reservation 
+    def add_reservation(reservation)
+      @reservations << reservation
+    end 
+
+   # Create the new reservation 
+    def reserve_room(start_date, end_date)
+      # start_date and end_date should be instances of class Date
+      given_date_range = Hotel::DateRange.new(start_date, end_date)
+      reservations_list = reservations.select do |res|
+        res.date_range.overlap?(given_date_range)
+      end
+      all_reserved_rooms = reservations_list.map do |res|
+        res.room
+      end
+
+      avialable_room = nil
+      rooms.each do |room|
+        if !(all_reserved_rooms).include?(room) 
+          avialable_room = room
+          break # stop when find an avialable_room 
+        end
+      end 
+
+      if avialable_room == nil 
+        raise ArgumentError.new " no room"
+      end
+
+
+      id = reservations.length + 1      
+      reservation = Reservation.new(id,given_date_range, avialable_room)
+      self.add_reservation(reservation)
+      return reservation
+
+    end
+
+    
+    # access the list of reservations for a specified room and a given date range
+    
+   
+    
+    
+    # access the list of reservations for a specific date, so that I can track reservations by date
     def reservations_list(date)
       reservation_list = reservations.select do |res|
         res.date_range.include?(date)
@@ -26,6 +73,12 @@ module Hotel
         return reservation_list
     end
 
+     # can get the total cost for a given reservation
+
+
+
+    # want exception raised when an invalid date range is provided, 
+    # so that I can't make a reservation for an invalid date range
     # Wave 2
     def available_rooms(start_date, end_date)
       # start_date and end_date should be instances of class Date
--- test/hotel_controller_test.rb
+++ test/hotel_controller_test.rb
@@ -21,9 +21,24 @@ describe Hotel::HotelController do
         expect(rooms.length).must_equal 20
       end
     end
+    describe "reservations_list_room_and_date" do
+      it "return a reservation list for a specified room and a given date range" do
+        room = Hotel::Room.new(1, 200)
+        start_date = Date.today
+        end_date = start_date + 3
+        date_range = Hotel::DateRange.new(start_date,end_date) 
+        # @reservation = Hotel::Reservation.new(1, date_range,room)    
+        reservations_list_of_given_room_date_range = @hotel_controller.reservations_list_room_and_date(room, date_range)
+
+        expect(reservations_list_of_given_room_date_range).must_be_kind_of Array
+        reservations_list_of_given_room_date_range.each do |res|
+          res.must_be_kind_of Hotel::Reservation
+        end
+      end
+    end
 
     describe "reserve_room" do
-      xit "takes two Date objects and returns a Reservation" do
+      it "takes two Date objects and returns a Reservation" do
         start_date = @date
         end_date = start_date + 3
 
@@ -31,6 +46,41 @@ describe Hotel::HotelController do
 
         expect(reservation).must_be_kind_of Hotel::Reservation
       end
+
+      it "The ability to reserve room when there are reservations with the same start_date and end_date" do
+        #Arrange 
+        start_date = Date.today
+        end_date = start_date + 4
+        reservation1 = @hotel_controller.reserve_room(start_date, end_date)
+        
+        # Action 
+        reservation2 = @hotel_controller.reserve_room(start_date, end_date)
+
+
+        # Assertion 
+        expect(@hotel_controller.reservations.length).must_equal 2
+        expect(reservation2.room.id).must_equal 2
+      end 
+
+      it " ########" do 
+        start_date = Date.today
+        end_date = start_date + 4
+
+        num_room = 0
+        while num_room < 20 do
+          @hotel_controller.reserve_room(start_date, end_date)
+          num_room +=1 
+        end
+      
+        # Action 
+        # reservation21 = @hotel_controller.reserve_room(start_date, end_date)
+        # p reservation21.room
+
+        expect{(@hotel_controller.reserve_room(start_date, end_date))}.must_raise ArgumentError
+      
+    end
+
+
     end
 
     describe "reservations" do
