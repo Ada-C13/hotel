9ab98114b410fbbca8f64082cff419a6bf4508e8
Vera <lizeth.veraro@outlook.com>
2020-03-05 22:36:29 -0800

    reservation intance variables(data_range and id). total_cost method

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,16 +1,25 @@
 
+require 'securerandom'
+
 module Hotel
 
   class Reservation
 
-    attr_reader :daterange, :room
+    attr_reader :daterange, :room, :id
     
     def initialize(daterange, room)
+      @daterange = daterange
       @room = room
+      @id = generate_id
     end
 
+    def generate_id
+      id = SecureRandom.hex(5)
+    end
+    
     def total_cost
-      return 2.0
+      total_cost = (daterange.nights * @room.cost)
+      return total_cost
     end
   end
 end
