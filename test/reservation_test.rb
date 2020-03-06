2e2c8150a2a3a45bc68e1b01e1f66e4c3c925c1b
dnguye2 <dnguyen1093@gmail.com>
2020-03-06 10:30:52 -0800

    fixed tests to deal with date objects only

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -1,13 +1,18 @@
 require_relative "test_helper"
 
+##### In this case we are not writing driver code. That means that your code should deal entirely in Ruby Date objects. Your tests should create Date objects and your library code should assume that it's receiving Date objects to start.
+
+##### When making tests you will want to use something like Date.new(1993, 2, 24) to create a date representing February 24, 1993 (or Date.today for today) instead of trying to parse a string or storing and re-parsing strings internally.
+
 describe Hotel::Reservation do
   describe "constructor" do
     before do
-      @reservation = Hotel::Reservation.new("20010203","20010206")
+      @reservation = Hotel::Reservation.new(Date.new(2017, 01, 05),Date.new(2017, 01, 06))
     end
     it "is an instance of reservation" do
       expect(@reservation).must_be_kind_of Hotel::Reservation
     end
+
 
     it "raises an argument error for negative-length ranges" do
       start_date = Date.new(2017, 01, 06)
