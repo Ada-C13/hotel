777190607c511658199d816dbb23e0f21dc2d889
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-03-06 16:44:20 -0800

    Add method find_all_reservations_by_date and corresponding tests

--- lib/hotel_dispatcher.rb
+++ lib/hotel_dispatcher.rb
@@ -10,6 +10,7 @@ module Hotel
       create_rooms
     end #initialize end
 
+    # Wave 1
     def create_rooms
       @rooms = []
         (1..20).each do |room_num|
@@ -20,8 +21,11 @@ module Hotel
         return @rooms
     end # create rooms end
 
-
+    # Wave 1
     def make_reservation(start_date, end_date)
+      if end_date < start_date 
+        raise ArgumentError
+      end
       room = create_rooms[0]
       new_reservation = Hotel::Reservation.new(start_date: start_date, end_date: end_date, room: room)
       @reservations << new_reservation
@@ -30,8 +34,6 @@ module Hotel
 
 
     def find_all_res_for_room(room_num, start_date, end_date)
-      start_date = Date.parse(start_date)
-      end_date = Date.parse(end_date)
       found_reservations = []
         @reservations.each  do |reservation| 
           next if reservation.room.room_num != room_num
@@ -46,27 +48,30 @@ module Hotel
     end # find_all_res_for_room end
 
 
-    def find_available_rooms(start_date, end_date)
-      start_date = Date.parse(start_date)
-      end_date = Date.parse(end_date)
-      found_rooms = []
-        @reservations.each do |reservation|
-          if (start_date >= reservation.end_date && end_date >= reservation.end_date) ||
-            (start_date < reservation.start_date && end_date <= reservation.start_date) ||
-            (start_date >= reservation.end_date && end_date > reservation.end_date)
-            found_rooms << reservation.room
-          else 
-            raise ArgumentError, "The dates you have entered are NOT available"
-          end
+    # def find_available_rooms(start_date, end_date)
+    #   found_rooms = []
+    #     @reservations.each do |reservation|
+    #       if (start_date >= reservation.end_date && end_date >= reservation.end_date) ||
+    #         (start_date < reservation.start_date && end_date <= reservation.start_date) ||
+    #         (start_date >= reservation.end_date && end_date > reservation.end_date)
+    #         found_rooms << reservation.room
+    #       else 
+    #         raise ArgumentError, "The dates you have entered are NOT available"
+    #       end
+    #     end
+    #     return found_rooms
+    # end # find_available_rooms end
+
+    # Wave 1
+    def find_all_reservations(date)
+      all_reservations_by_date = []
+      @reservations.each do |reservation|
+        if (date >= reservation.start_date && date < reservation.end_date)
+          all_reservations_by_date << reservations
         end
-        return found_rooms
-    end # find_available_rooms end
-
-
-  # def all_reservations(date)
-  # all_reservations_by_date = []
-  # return all_reservations_by_date
-  # end
+      end
+      return all_reservations_by_date
+    end #find_all_reservations by date end
 
 
   end # class end
--- test/hotel_dispatcher_test.rb
+++ test/hotel_dispatcher_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require 'pry'
 
 describe "HotelDispatcher class" do
   describe "initialize" do
@@ -19,73 +20,103 @@ describe "HotelDispatcher class" do
   describe "make a new reservation" do
     it "@reservations should be length one" do
       dispatcher = Hotel::HotelDispatcher.new()
-      expect(dispatcher.make_reservation("March 1, 2019", "March 5, 2019").length).must_equal 1
+      dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+      expect(dispatcher.reservations.length).must_equal 1
     end
 
     it "is an array" do
     dispatcher = Hotel::HotelDispatcher.new()
-    expect(dispatcher.make_reservation("March 1, 2019", "March 5, 2019")).must_be_kind_of Array
+    expect(dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))).must_be_kind_of Array
     end
+
+    it "raises ArgumentError when start date is greater than end date" do
+      dispatcher = Hotel::HotelDispatcher.new()
+      expect{ dispatcher.make_reservation(Date.new(2019, 03, 15), Date.new(2019, 03, 01)) }.must_raise ArgumentError
+    end
+
   end #make a new reservation end
+
 
   describe "find all reservations by room num and date range" do
 
     it "finds all reservations for a room given room_num and date range" do
       dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_all_res_for_room(1, "March 3, 2019", "March 7, 2019")).must_be_kind_of Array
+      dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+      expect(dispatcher.find_all_res_for_room(1, Date.new(2019, 03, 03), Date.new(2019, 03, 07))).must_be_kind_of Array
     end
 
     it "returns a reservation when the given start_date falls into the reservation date range" do 
       dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_all_res_for_room(1, "March 4, 2019", "March 10, 2019").length).must_equal 1
+      dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+      expect(dispatcher.find_all_res_for_room(1, Date.new(2019, 03, 04), Date.new(2019, 03, 10)).length).must_equal 1
     end
 
     it "returns a reservation when the given end_date falls into the reservation date range" do 
       dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_all_res_for_room(1, "Feb 28, 2019", "March 4, 2019").length).must_equal 1
+      dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+      expect(dispatcher.find_all_res_for_room(1, Date.new(2019, 02, 28), Date.new(2019, 03, 03)).length).must_equal 1
     end
 
     it "does NOT return a reservation when the given date_ranges do NOT overlap" do 
       dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_all_res_for_room(1, "March 7, 2019", "March 10, 2019")).must_be_empty
+      dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+      expect(dispatcher.find_all_res_for_room(1, Date.new(2019, 03, 07), Date.new(2019, 03, 10))).must_be_empty
     end
 
   end # describ block end
 
-  describe "find available rooms" do
+  # describe "find available rooms" do
 
-    it "finds available rooms when reservation end_date and requested new room start_date do not overlap" do
-      dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_available_rooms("March 6, 2019", "March 10, 2019").length).must_equal 1
-    end
+  #   it "will have 20 rooms on a new Hotel Dispatcher object" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 03, 06), Date.new(2019, 03, 10)).length).must_equal 20
+  #   end
 
-    it "finds available rooms by open dates" do
-      dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_available_rooms("February 27, 2019", "March 1, 2019").length).must_equal 1
-    end
+  #   it "will return 19 available rooms only if there is one existing reservation that falls within the given date range" do 
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 03, 01), Date.new(2019, 03, 10)).length).must_equal 20
 
-    it "finds available rooms by open dates" do
-      dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_available_rooms("March 5, 2019", "March 10, 2019").length).must_equal 1
-    end
 
-    it "finds available rooms by open dates" do
-      dispatcher = Hotel::HotelDispatcher.new()
-      dispatcher.make_reservation("March 1, 2019", "March 5, 2019")
-      expect(dispatcher.find_available_rooms("February 27, 2019", "March 1, 2019").length).must_equal 1
-    end
+  #   end
+
+  #   it "when the date ranges for the reservation and the args for the make_res_method do not overlap" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 03, 06), Date.new(2019, 03, 10)).length).must_equal 1
+  #   end
+
+  #   it "finds available rooms by open dates" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 02, 27), Date.new(2019, 03, 01)).length).must_equal 1
+  #   end
+
+  #   it "finds available rooms by open dates" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 03, 05), Date.new(2019, 03, 10)).length).must_equal 1
+  #   end
+
+  #   it "finds available rooms by open dates" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 05))
+  #     expect(dispatcher.find_available_rooms(Date.new(2019, 02, 27), Date.new(2019, 03, 01)).length).must_equal 1
+  #   end
+
+  #   it "finds all reservations for a single date" do
+  #     dispatcher = Hotel::HotelDispatcher.new()
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 07))
+  #     dispatcher.make_reservation(Date.new(2019, 03, 06), Date.new(2019, 03, 10))
+  #     dispatcher.make_reservation(Date.new(2019, 03, 01), Date.new(2019, 03, 06))
+  #     expect(dispatcher.find_all_reservations(Date.new(2019, 03, 06)).length).must_equal 2
+  #     expect(dispatcher.find_all_reservations(Date.new(2019, 04, 01)).length).must_equal 0
+  #   end
 
    
 
 
-  end # describe block end
+  # end # describe block end
 
 
 
