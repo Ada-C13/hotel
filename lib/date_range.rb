56f53766d63e4c32043f349585e73f96a83a462a
Rosalyn <syrosalynyu@gmail.com>
2020-03-06 17:32:14 -0800

    Removed reservation_id since it is not required

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -9,17 +9,20 @@ module Hotel
       unless start_date <= end_date
         raise ArgumentError.new("End date can't be earlier than start date (got start date: #{start_date} ; end date: #{end_date})")
       end
-
-      # check if the provided start and end dates are both a valid date (ie "2020-01-32")
-      first_date = start_date.split("-").map {|item| item.to_i}
-      last_date = end_date.split("-").map {|item| item.to_i}
-      if !(Date.valid_date?(first_date[0], first_date[1], first_date[2])) || !(Date.valid_date?(last_date[0], last_date[1], last_date[2]))
-        raise ArgumentError.new("Date(s) you provided is/are invalid")
-      end
-      
+      date_validation(start_date, end_date)
       @start_date = Date.parse(start_date) 
       @end_date = Date.parse(end_date) 
     end
+
+    def date_validation(start_date, end_date)
+     # check if the provided start and end dates are both a valid date (ie "2020-01-32")
+     first_date = start_date.split("-").map {|item| item.to_i}
+     last_date = end_date.split("-").map {|item| item.to_i}
+      if !(Date.valid_date?(first_date[0], first_date[1], first_date[2])) || !(Date.valid_date?(last_date[0], last_date[1], last_date[2]))
+        raise ArgumentError.new("Date(s) you provided is/are invalid")
+      end
+    end
+
 
     def nights
       return self.dates_exclude_last.length
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -3,25 +3,12 @@ require_relative "test_helper"
 describe "Reservation" do
   describe "#initialize" do
     before do
-      id = 1
       date_range = Hotel::DateRange.new("2020-3-13", "2020-3-15")
       room_num = 15
       @reservation = Hotel::Reservation.new(
-        id, 
         date_range,
         room_num
       )
-    end
-    
-
-    it "id must be a positive integer" do
-      expect(@reservation.id).must_be_kind_of Integer
-      expect(@reservation.id).must_equal 1
-    end
-
-    it "raise ArgumentError if id is not a positive integer" do
-      id = -100
-      expect{Hotel::Reservation.new(@reservation)}.must_raise ArgumentError
     end
 
     it "date_range must be a instance of DateRange class" do
