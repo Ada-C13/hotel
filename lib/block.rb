feaf78473ea7b20f6bf0a311a8f9a4d19ef3b0a8
Yaz <yasmineoshaugh@gmail.com>
2020-03-06 21:38:35 -0800

    Further fleshed out Block constructor and completed Block#reserve(room). Wrote extensive tests in block_tests.rb.

--- lib/block.rb
+++ lib/block.rb
@@ -1,25 +1,42 @@
 module Hotel
   class Block < DateRange
+    attr_reader :available_rooms, :reservations, :rate, :available_rooms
     def initialize(start_date, end_date, rooms, rate)
+      if rooms.length > 5
+        raise ArgumentError.new("Blocks can only be made for 5 rooms or less.")
+      end
       super(start_date, end_date)
-      # range necessary for class state?, or local var better?
-      @range = DateRange.new(start_date, end_date)
-      if rooms.any? { |room| !(room.available?(@range))}
-        raise ArgumentError.new("Room #{room.id} is not available at given date range.")
+      range = DateRange.new(start_date, end_date)
+      if rooms.any? { |room| !(room.available?(range))}
+        raise ArgumentError.new("One of the rooms is not available at given date range.")
       end
       @rate = rate
       @reservations = []
-      @rooms = rooms
-      @rooms.each do |room|
+      @available_rooms = rooms
+      @available_rooms.each do |room|
         room.add(self)
       end
-
     end
 
     def reserve(room)
-    end
-
-    def available_rooms
+      if !(available_rooms.include?(room))
+        raise ArgumentError.new("Room #{room.id} is not in this Block.")
+      end
+      if available_rooms.all?{|room| room == nil}
+        raise ArgumentError.new("All rooms in block are already reserved.")
+      end
+      new_res = Reservation.new(start_date, end_date, room.id, rate)
+      self.reservations << new_res
+      # finds the block in room's reservations, turns it into a reservation
+      # TO DO: is the first conditional necessary?
+      i = room.reservations.find_index {|reservation|
+        reservation.class == Block && reservation.nights == self.nights
+      }
+      room.reservations[i] = new_res
+      j = available_rooms.find_index {|r|
+        r == room
+      }
+      available_rooms[j] = nil
     end
 
 
--- test/block_test.rb
+++ test/block_test.rb
@@ -1,5 +1,103 @@
 require_relative "test_helper"
 
 describe Hotel::Block do
-  
+  before do
+    @fd = Hotel::FrontDesk.new
+    @rooms = @fd.rooms.slice(0,5)
+    @start = Date.new(2020, 01, 01)
+    @end = Date.new(2020, 01, 07)
+    @rate = 153
+  end
+
+  describe "constructor" do
+    describe "has all the expected state" do
+      it "rooms" do 
+        block = Hotel::Block.new(@start, @end, @rooms, @rate)
+        expect(block.available_rooms).must_be_kind_of Array
+        avail_rooms = block.available_rooms
+        avail_rooms.each_with_index do |room, i|
+          expect(room).must_equal @rooms[i]
+        end
+      end
+      it "reservations" do
+        block = Hotel::Block.new(@start, @end, @rooms, @rate)
+        expect(block.reservations).must_be_kind_of Array
+      end
+    end
+    it "raises error if a room in rooms is not available" do
+      @rooms[4].add(Hotel::Reservation.new(@start, @end, 5, 150))
+      expect{Hotel::Block.new(@start, @end, @rooms, @rate)}.must_raise ArgumentError
+    end
+    it "raises an error if rooms is > 5" do
+      @rooms << Hotel::Room.new(6)
+      expect{Hotel::Block.new(@start, @end, @rooms, @rate)}.must_raise ArgumentError
+    end
+    it "reflects changes in rooms included in block" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each do |room|
+        expect(room.reservations.include?(block)).must_equal true
+      end
+    end
+    
+  end
+
+  describe "reserve(room)" do
+    it "raises error if attempting to reserve a room not in block" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      expect{block.reserve(Hotel::Room.new(20))}.must_raise ArgumentError
+    end
+
+    it "reflects changes in class state: reservations" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each do |room|
+        block.reserve(room)
+      end
+      block.reservations.each_with_index do |reservation, i|
+        expect(reservation.room).must_equal i + 1
+      end
+    end
+
+    it "reserved room no longer has the block in their reservations, but a reservation instead" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each do |room|
+        block.reserve(room)
+        expect(room.reservations.any?(Hotel::Block)).must_equal false
+      end
+      
+    end
+    it "won't erase another block in making room's block>reservation change" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      start2 = Date.new(2020, 02, 01)
+      end2 = Date.new(2020, 02, 28)
+      block2 = Hotel::Block.new(start2, end2, @rooms, @rate)
+      @rooms.each do |room|
+        block.reserve(room)
+        expect(room.reservations.find{|r| r.class == Hotel::Block}).must_equal block2
+      end
+    end 
+    it "removes room from available_rooms" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each_with_index do |room, i|
+        block.reserve(room)
+        expect(block.available_rooms.any?{|r| r == room }).must_equal false
+      end
+    end
+    it "effectively adds discounted rate to reservation" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each_with_index do |room, i|
+        block.reserve(room)
+      end
+      block.reservations.each do |r|
+        expect(r.cost % 153 == 0).must_equal true
+      end
+    end
+    it "raises error if all rooms are booked" do
+      block = Hotel::Block.new(@start, @end, @rooms, @rate)
+      @rooms.each_with_index do |room, i|
+        block.reserve(room)
+      end
+      expect{block.reserve(@rooms[0])}.must_raise ArgumentError
+    end
+  end
+
 end
