ef1b19c2edeb84b4f56133f8ac2e4b0b0510a2da
dnguye2 <dnguyen1093@gmail.com>
2020-03-07 10:36:31 -0800

    fixed valid_room_inputs array

--- lib/hotel_manager.rb
+++ lib/hotel_manager.rb
@@ -3,8 +3,6 @@ module Hotel
     # Wave 1
     attr_reader :rooms
     def rooms
-      # You might want to replace this method with an attr_reader
-      # array of hashes for rooms
       @rooms = []
       20.times do |i|
         @rooms << {(("room#{i+1}").to_sym) => []}
@@ -18,11 +16,14 @@ module Hotel
         # covered by having Reservation inherit from DateRange
       # need to keep in mind implementation of room choice
 
+
+      #TODO
       # room chosen is based on the method! not by the user! (see README.md)
       # if nil, default will then redirect to be first available room
       # add reservation instance to @rooms array based off of room attribute
       # add date instance to @rooms array
-      return Reservation.new(start_date, end_date)
+      reserved_room = Hotel::DateRange.new(start_date, end_date)
+      return Reservation.new(reserved_room.start_date, reserved_room.end_date)
     end
 
     def reservations(date)
@@ -45,6 +46,25 @@ module Hotel
     def reservations_by_room(room, date)
     # to lookup by room use key
     # use reservations(date) method to get specific date range
+    valid_room_inputs = []
+    20.times do |i|
+      valid_room_inputs << ("room#{i+1}").to_sym
+    end
+
+    unless valid_room_inputs.include?(room)
+      raise ArgumentError.new("Not a valid room")
+    end
+
+    room_index = ((room.to_s).match('[0-9]')[0]).to_i
+
+    return []
+
+    # room 1 - @rooms[0][:room1]
+    # room 5 - @rooms[4][:room5]
+    # use regex on room attribute, then turn into integer subtract 1 for the index to access
+
+
+
     end
 
     # Wave 2
