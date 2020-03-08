5ab6497be253565a3a19d0f8691a2ccd4abeca5b
Nikki Vaughan <vnikki13@gmail.com>
2020-03-07 16:17:47 -0800

    added an optional block parameter when making a reservation. Reader for it

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,12 +1,13 @@
 module Hotel
   class Reservation < DateRange
-    attr_reader :room_num, :nights, :total_cost
+    attr_reader :room_num, :nights, :total_cost, :block_id
 
-    def initialize(arrive, depart, room)
+    def initialize(arrive, depart, room, block_id = nil)
       super(arrive, depart)
       @room_num = room.room_num
       @nights = nights
       @total_cost = @nights * room.rate
+      @block_id = block_id
     end
 
     def nights
