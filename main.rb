def524f50caabdfeb5efb552d1628ed9e0764f6e
cojenco <icathyouyang@gmail.com>
2020-03-08 12:40:26 -0700

    Added small driver methods to the main.rb file.

--- main.rb
+++ main.rb
@@ -27,16 +27,42 @@ def create_date
   return new_date
 end
 
+def create_start_date
+  puts "Please enter start date in DD/MM/YYYY format"
+  stringdate = gets.chomp
+  new_date = Date.parse(stringdate)
+  return new_date
+end
+
+def create_end_date
+  puts "Please enter end date in DD/MM/YYYY format"
+  stringdate = gets.chomp
+  new_date = Date.parse(stringdate)
+  return new_date
+end
+
 def ask_room
   puts "Please enter the Room ID"
-  room_id = gets.chomp
+  room_id = gets.chomp.to_i
   return room_id
+end
+
+def ask_block
+  puts "Please enter the Block ID"
+  block_id = gets.chomp.to_i
+  return block_id
 end
 
 def ask_rate
   puts "Please enter the rate"
   rate = gets.chomp
   return rate.to_f
+end
+
+def ask_quantity
+  puts "Please enter the quantity of rooms for the hotel block"
+  quantity = gets.chomp.to_i
+  return quantity
 end
 
 def list_reservations(reservations)
@@ -57,6 +83,7 @@ def list_blocks(blocks_array)
   end
 end
 
+
 def main
   coordinator = Hotel::SystemCoordinator.new
 
@@ -74,56 +101,49 @@ def main
         bookings = coordinator.find_reservations_by_date(date_given)
         list_reservations(bookings)
       when "3", "3.", "find reservations by room and date range"
-        start_date = create_date
-        end_date = create_date
+        start_date = create_start_date
+        end_date = create_end_date
         range_given = Hotel::DateRange.new(start_date, end_date)
         room_id = ask_room
         bookings = coordinator.find_reservations_room_date(room_id.to_i,range_given)
         list_reservations(bookings)
       when "4", "4.", "find available rooms for date range"
-        start_date = create_date
-        end_date = create_date
+        start_date = create_start_date
+        end_date = create_end_date
         range_given = Hotel::DateRange.new(start_date, end_date)
-        rooms = coordinator.find_available_rooms(range_given)
-        puts "There are #{rooms.length} rooms available."
-        rooms.list_rooms
-        # rooms.each do |room|
-        #   print "Room #{room.room_id} | "
-        # end
+        rooms_found = coordinator.find_available_rooms(range_given)
+        puts "There are #{rooms_found.length} rooms available."
+        list_rooms(rooms_found)
       when "5", "5.", "find available rooms for hotel block"
-        start_date = create_date
-        end_date = create_date
-        range_given = Hotel::DateRange.new(start_date, end_date)
-        rooms = coordinator.find_block_rooms(range_given)
-        puts "There are #{rooms.length} rooms available."
-        rooms.list_rooms
+        block_given = ask_block
+        availability = coordinator.check_block_availability(block_given)
+        # puts availability
+        puts availability == true ? "Available rooms in this block" : "No vacant rooms in this block"
       when "6", "6.", "make specific hotel block"
-        start_date = create_date
-        end_date = create_date
+        start_date = create_start_date
+        end_date = create_end_date
         range_given = Hotel::DateRange.new(start_date, end_date)
+        quantity = ask_quantity
+        while quantity > 5 || quantity <= 0
+          quantity = ask_quantity
+        end
         room_array = []
-        5.times do
-          room_id = ask_room
+        quantity.times do
+          room_id = ask_room.to_i
           room_array << room_id
         end
         cost = ask_rate
         block = coordinator.make_specific_block(range_given, room_array, cost)
-        block.list_blocks
+        list_blocks(block)
       when "7", "7.", "make reservation"
-        start_date = create_date
-        end_date = create_date
+        start_date = create_start_date
+        end_date = create_end_date
         reservation = coordinator.make_reservation(start_date,end_date)
-        puts "Reservation is made. It is room #{reservation.room_id} from #{reservation.start_date} to #{reservation.end_date}"
+        puts "Reservation is made. It is Room #{reservation.room_id} from #{reservation.start_date} to #{reservation.end_date}"
       when "0", "0.", "exit", "quit", "0. exit"
         control_loop = false
     end
   end
-
 end
-
-# example = Date.parse("10/10/2020")
-# puts example.class 
-
-# puts example
 
 main
