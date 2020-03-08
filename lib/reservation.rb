32ae361e3125a00bf5cc3e345fea15c3c9ddfecf
Katie <katie.vandervoort@gmail.com>
2020-03-08 14:50:51 -0700

    changed reservation to be a parent class instead of to actually reserve rooms

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,23 +1,15 @@
 require 'date'
 
 require_relative 'date_range.rb'
-require_relative 'room.rb'
 
 module Hotel
   class Reservation
     attr_reader :start_date, :end_date, :room, :date_range
     
-    def initialize(start_date, end_date, room)
-      raise ArgumentError.new("room must be an instance of room") if room.class != Hotel::Room
-
+    def initialize(start_date, end_date)
       @start_date = start_date
       @end_date = end_date
-      @room = room # instance of room class
       @date_range = Hotel::DateRange.new(start_date, end_date)
-    end
-
-    def cost
-      return Hotel::DateRange.new(@start_date, @end_date).nights * @room.cost
     end
 
   end
