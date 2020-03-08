ebf2cde1ca0988f648bda550b8824f39efbb39c4
Katie <katie.vandervoort@gmail.com>
2020-03-07 20:14:11 -0800

    created block and block test files and added to test helper

---
+++ lib/block.rb
@@ -0,0 +1,13 @@
+require 'date'
+
+require_relative 'date_range.rb'
+require_relative 'room.rb'
+require_relative 'reservation.rb'
+
+module Hotel
+  class Block < Reservation
+
+
+
+  end
+end
---
+++ test/block_test.rb
@@ -0,0 +1,17 @@
+require_relative "test_helper"
+
+describe Hotel::Block do
+  before do
+   
+  end
+
+  
+  describe "initializes block correctly" do
+
+    it "block can be succesfully created" do
+
+    end
+
+  end
+
+end
