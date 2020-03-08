86005973fbdd9ebbf41d7721415c7283f60254a1
Nikki Vaughan <vnikki13@gmail.com>
2020-03-07 16:22:03 -0800

    added a parameter for number of rooms and now have a date_range as opposed to an arrive and depart

--- lib/block.rb
+++ lib/block.rb
@@ -1,10 +1,11 @@
 module Hotel
   class Block
-    attr_reader :arrive, :depart, :rate
-    def initialize(arrive, depart, rate)
-      @arrive = arrive
-      @depart = depart
+    attr_reader :date_range, :rate, :num_of_rooms
+    
+    def initialize(arrive, depart, rate, num_of_rooms)
+      @date_range = Hotel::DateRange.new(arrive, depart)
       @rate = rate
+      @num_of_rooms = num_of_rooms
     end
   end
 end
