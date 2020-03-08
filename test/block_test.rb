dd5e0b091cf047bf35f3d4a0467ac460ba00eabe
Nikki Vaughan <vnikki13@gmail.com>
2020-03-07 16:25:45 -0800

    made new tests for date_range change

--- test/block_test.rb
+++ test/block_test.rb
@@ -5,21 +5,18 @@ describe 'Block class' do
     @arrive = Date.new(2021, 2, 24)
     @depart = Date.new(2021, 2, 26)
     @rate = 150
-    @new_block = Hotel::Block.new(@arrive, @depart, @rate)
+    @three_rooms = 3
+    @new_block = Hotel::Block.new(@arrive, @depart, @rate, @three_rooms)
   end
 
   describe 'Block makes a new instance of block class' do
     it 'must make a new block' do
-      expect(Hotel::Block.new(@arrive, @depart, @rate)).must_be_instance_of Hotel::Block
-      expect(@new_block.arrive).must_equal @arrive
-      expect(@new_block.depart).must_equal @depart
+      expect(Hotel::Block.new(@arrive, @depart, @rate, @three_rooms)).must_be_instance_of Hotel::Block
+      expect(@new_block.date_range).must_be_instance_of Hotel::DateRange
+      expect(@new_block.date_range.arrive).must_equal @arrive
+      expect(@new_block.date_range.depart).must_equal @depart
       expect(@new_block.rate).must_equal @rate
+      expect(@new_block.num_of_rooms).must_equal @three_rooms
     end
-  end
-
-  describe 'given a range of dates determine if x amount of rooms can be booked' do 
-  end
-
-  describe 'blocks should be viewed with other reservatiosn' do 
   end
 end
