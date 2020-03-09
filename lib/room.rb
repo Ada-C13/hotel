c7cd0313c0d1eccabff7c47546ffb6e5a70bb742
Yesenia <yesentorres@gmail.com>
2020-03-08 17:04:42 -0700

    Update room and room tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,1 +1,18 @@
-CONSTANT = 24
+module Hotel 
+  class Room
+
+    attr_accessor :id, :cost, :reservation_ids
+
+    VALID_NUMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] # hotel has 20 rooms
+
+    def initialize(number:, cost: 200.00, reservation_ids: [])
+      if VALID_NUMS.include?(number) == false 
+        raise ArgumentError.new("Room ##{id} does not exist")
+      end 
+      @number = number
+      @cost = cost 
+      @reservation_ids = reservation_ids
+    end
+
+  end 
+end  
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,11 +1,28 @@
 require_relative "test_helper"
 
-describe "dummy tests" do 
-  it "should run a failed test" do 
-    _(true).must_equal false
-  end
+describe "Room Class" do
 
-  it "should be able to access data from room.rb" do 
-    _(CONSTANT).must_equal 24
+  describe "initialize" do
+    # Assumptions: default cost of room is $200.00 and there's only 20 rooms 
+    it "creates an instance of Room object" do 
+      @test_room = Hotel::Room.new(number: 1)
+      expect(@test_room).must_be_kind_of Hotel::Room
+    end 
+
+    it "sets cost to 200.00 as default" do 
+      @test_room = Hotel::Room.new(number: 1)
+      expect(@test_room.cost).must_equal 200.00
+    end 
+
+    it "sets reservation_ids array to empty as default" do 
+      @test_room = Hotel::Room.new(number: 1)
+      expect(@test_room.reservation_ids.length).must_equal 0
+    end 
+
+    it "raises an error if valid room_id is not given" do
+      expect{Hotel::Room.new(number: 0)}.must_raise ArgumentError
+      expect{Hotel::Room.new(number: 100)}.must_raise ArgumentError
+    end
   end 
-end 
+  
+end  
