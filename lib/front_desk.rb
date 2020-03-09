5537aee3ddfcd71695606c9f3ed7739bc2501a59
Vera <lizeth.veraro@outlook.com>
2020-03-08 21:22:59 -0700

    Comments and code organizing

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -24,20 +24,56 @@ module Hotel
       return @reservations
     end
 
+    
     def get_available_rooms(date_range)
       unavailable_rooms = []
-    
+      # Send the room to unavailable rooms if has a reservation for a specific data_range. 
       @reservations.each do |reservation|
         if reservation.daterange.overlap?(date_range) == true
           unavailable_rooms << reservation.room
         end
       end
-
+      # Array substrating total_rooms minus unavailable rooms.
       available_rooms = @rooms - unavailable_rooms
-
       return available_rooms
-
     end
+
+    def reservations_by_room_date (room, date_range)
+      # Rooms contains all the reservations match based on the room given. 
+      rooms = @reservations.select do |reveservation|
+        reveservation.room.room_id == room
+      end
+      reservations_by_room = rooms.select do |reservation|
+        reservation.daterange == date_range
+      end
+
+      if reservations_by_room.empty?
+        raise ArgumentError
+      end
+
+      return reservations_by_room
+    end
+
+    def reservations_by_date(date)
+      total_by_date =  @reservations.select do |reservation|
+        reservation.daterange.start_date == date
+      end
+
+      if total_by_date.empty?
+        raise ArgumentError
+      end
+      return total_by_date
+    end
+
+    def cost_by_reservation(reservation_number)
+      cost = nil
+      @reservations.each do |reservation|
+         if reservation.id == reservation_number
+          cost = reservation.total_cost
+         end
+       end
+       return cost
+     end
 
     def make_resevation(date_range)
 
@@ -51,38 +87,6 @@ module Hotel
       return new_reservation
     end
 
-    def reservations_by_date(date)
-      total_by_date =  @reservations.select do |reservation|
-        reservation.start_date == date
-      end
-      return total_by_date
-    end
-    
-    def reservations_by_room_date (room, date_range)
 
-      rooms = @reservations.select do |reveservation|
-        reveservation.room.room_id == room
-      end
-
-      reservations_by_room = rooms.select do |reservation|
-        reservation.daterange == date_range
-      end
-
-      if reservations_by_room.empty?
-        raise ArgumentError
-      end
-
-      return reservations_by_room
-    end
-
-    def cost_by_reservation(reservation_number)
-     cost = nil
-     @reservations.each do |reservation|
-        if reservation.id == reservation_number
-         cost = reservation.total_cost
-        end
-      end
-      return cost
-    end
   end
 end
