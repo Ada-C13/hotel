6a8aea3ee5370bab022e55138885cd2d08357769
denisseai <msdenisseanaya@gmail.com>
2020-03-08 20:54:59 -0700

    Added reservations, list_rooms as arrays and tests

--- lib/room_block.rb
+++ lib/room_block.rb
@@ -1,18 +1,24 @@
 module Hotel
   class RoomBlock
-    attr_reader :name, :start_date, :end_date, :discount, :block_id
+    attr_reader :name, :start_date, :end_date, :discount, :how_many_rooms, :list_rooms
 
-    def initialize(name, start_date, end_date, discount, block_id)
+    def initialize(name, start_date, end_date, discount, how_many_rooms, list_rooms)
       @name = name
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
       @discount = discount / 100.0
-      @block_id = block_id
-
+      @how_many_rooms = how_many_rooms
+      @list_rooms = list_rooms
+    
       # Check for requirements and raise error if not met for dates
       raise ArgumentError.new("The end date cannot be before the start date") if @start_date >= @end_date
-      # Check for requirements and raise error if not met for room's block
-      raise ArgumentError.new("Invalid amount of rooms for block") if block_id <= 0 || block_id > 5
+      # Check for requirements and raise error if block of rooms does not pass
+      raise ArgumentError.new("Invalid amount of rooms for block") if how_many_rooms <= 0 || how_many_rooms > 5
+    end
+
+    # Set as empty array if no instance variable was instantiated
+    def reservations
+      @reservations ||= []
     end
   end
 end
--- test/room_block_test.rb
+++ test/room_block_test.rb
@@ -1,12 +1,12 @@
 require_relative "test_helper"
 
 describe "RoomBlock" do
-  let (:room1) { Hotel::Room.new(1) }
-  let (:room2) { Hotel::Room.new(2) }
-  let (:room3) { Hotel::Room.new(3) }
-  let (:room4) { Hotel::Room.new(4) }
-  let (:room5) { Hotel::Room.new(5) }
-  let (:block) { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 3) }
+  let (:block_room_1) { Hotel::Room.new(1) }
+  let (:block_room_2) { Hotel::Room.new(2) }
+  let (:block_room_3) { Hotel::Room.new(3) }
+  let (:block_room_4) { Hotel::Room.new(4) }
+  let (:block_room_5) { Hotel::Room.new(5) }
+  let (:block) { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 3,[block_room_1, block_room_2, block_room_3]) }
   # Arrange
   describe "initialize" do  
   # Act
@@ -33,30 +33,45 @@ describe "RoomBlock" do
       expect(block.end_date).must_equal Date.parse("March 5, 2020")
     end
     # Act
-    it "Keep track of discount" do
+    it "discount tracker" do
       # Assert
       expect(block).must_respond_to :discount
       expect(block.discount).must_equal 0.50
     end
     # Act
-    it "Keep track of block_id" do
+    it "how_many_rooms' tracker" do
       # Assert
-      expect(block).must_respond_to :block_id
-      expect(block.block_id).must_equal 3
+      expect(block).must_respond_to :how_many_rooms
+      expect(block.how_many_rooms).must_equal 3
     end
     # Act
     it "1 - 5 rooms allowed per block" do
-      block_a = Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 1)
-      block_b = Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 5)
+      block_a = Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 1, [block_room_1])
+      block_b = Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 5, [block_room_1, block_room_2, block_room_3, block_room_4, block_room_5])
       # Assert
-      expect(block_a.block_id).must_equal 1
-      expect(block_b.block_id).must_equal 5
+      expect(block_a.how_many_rooms).must_equal 1
+      expect(block_b.how_many_rooms).must_equal 5
     end
     # Act
     it "Raise error for blocks of rooms that don't meet requirements" do
       # Assert
-      expect { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 0) }.must_raise ArgumentError
-      expect { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 6) }.must_raise ArgumentError
+      expect { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 0, [block_room_1]) }.must_raise ArgumentError
+      expect { Hotel::RoomBlock.new("Anaya", "March 3, 2020", "March 5, 2020", 50, 6, [block_room_1]) }.must_raise ArgumentError
+    end
+    # Act
+    it "list_rooms' tracker" do
+      # Assert
+      expect(block).must_respond_to :list_rooms
+      expect(block.list_rooms).must_be_kind_of Array
+      expect(block.list_rooms.include?(block_room_1)).must_equal true
+      expect(block.list_rooms.include?(block_room_2)).must_equal true
+      expect(block.list_rooms.include?(block_room_3)).must_equal true
+    end
+    # Act
+    it "reservations' tracker" do
+      # Assert
+      expect(block).must_respond_to :reservations
+      expect(block.reservations).must_be_kind_of Array
     end
   end
 end
