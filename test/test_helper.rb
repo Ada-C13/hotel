9bb12510e9b47cbd64a7745d5cc7ba5580b6a641
thenora <thenora@gmail.com>
2020-03-08 22:15:46 -0700

    updated test helper to remove rooms

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,6 @@ require "date"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative "../lib/room"
 require_relative "../lib/front_desk"
 require_relative "../lib/reservation"
 require_relative "../lib/date_range"
