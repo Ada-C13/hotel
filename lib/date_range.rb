93145f23b141d6726247270d4153b6ee91c5de1a
Vera <lizeth.veraro@outlook.com>
2020-03-09 00:05:52 -0700

    Overlap? refactoring and comments

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -10,20 +10,13 @@ module Hotel
       @end_date = end_date
     end
 
+    # An input range given will not be part of any other date_range
+    # It returns true if there is an overlap or false if there is not.
+    # input_range = Hotel::DataRange instance.
     def overlap?(input_range)
-
       a = self.start_date >= input_range.end_date
       b = self.end_date <= input_range.start_date
-
-      if a || b
-        return false
-      else
-        return true
-      end
-    end
-
-    def include?(input_range)
-  
+      return a || b ? false : true 
     end
 
     def nights
@@ -32,7 +25,9 @@ module Hotel
     end
 
     private
-
+    # validate_date? validates that start and end date have valid format.
+    # start date cannot be after the end date 
+    # Exception raised when an invalid date range is provided
     def self.validate_date?(start_date, end_date)
 
       date_format = '%Y-%m-%d'
@@ -40,8 +35,12 @@ module Hotel
         raise ArgumentError
       end
 
-      if start_date > end_date || start_date - end_date == 0
+      if start_date > end_date
         raise ArgumentError.new("The end time is before the start time.")
+      end
+
+      if start_date - end_date == 0
+        raise ArgumentError.new("Start date and end date are the same.")
       end
     end
   end 
