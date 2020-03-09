18338dd64763688e8f3207827394b17533a51c0c
Vera <lizeth.veraro@outlook.com>
2020-03-08 18:00:37 -0700

    Refactoring and comments

--- test/front_desk_test.rb
+++ test/front_desk_test.rb
@@ -1,6 +1,7 @@
 require_relative 'test_helper'
 
 describe Hotel::FrontDesk do 
+  # Intance of front_desk and date_range created before. Use them later to test.
   before do 
     @front_desk = Hotel::FrontDesk.new
     @date = Date.new(2020,05,21)
@@ -15,12 +16,14 @@ describe Hotel::FrontDesk do
         rooms = @front_desk.room_list
         expect(rooms).must_be_kind_of Array
       end
-      it "Raises an error if the array of rooms it is empty" do
+
+      it "raises an error if the array of rooms it is empty" do
         rooms = @front_desk.room_list.length
         expect(rooms).must_be :>, 1
       end
 
       it "raises an error if there are not available rooms" do
+        # 20 rooms created to test the case with not available dates.
         20.times do
           new_reservation = @front_desk.make_resevation(@date_range)
         end
@@ -36,45 +39,46 @@ describe Hotel::FrontDesk do
     end
 
     describe "Reservations" do
+      # New reservations created before to use in all cases of reservations.
+      before do 
+        @new_reservation = @front_desk.make_resevation(@date_range)
+      end
+
       it "returns an array of reservations" do
         reservations = @front_desk.total_reservations
-      
+        # Every reservation within the array should be a instance of reservation.
         reservations.each do |reservation|
-          reservation.must_be_kind_of Reservation
+          reservation.must_be_kind_of Hotel::Reservation
         end
-
         expect(reservations).must_be_kind_of Array
       end
-    end
 
-    describe "Reservations_by_date" do
       it "returns an a array of reservations by date" do
-        date = Date.new(2017,12,04)
+        date = Date.new(2020,05,21)
         total_by_date = @front_desk.reservations_by_date(date)
         expect(total_by_date).must_be_kind_of Array
       end
-    end
 
-    describe "reservations_by_room_date" do
+      it "raises an error if there are not reservations by a specific date" do
+        date = Date.new(2020,05,22)
+        expect{ @front_desk.reservations_by_date(date) }.must_raise ArgumentError
+      end
+
       it "returns a list of reservations for a specified room and a given date range" do
-        new_reservation = @front_desk.make_resevation(@date_range)
         search = @front_desk.reservations_by_room_date(1, @date_range)
         expect(search).must_be_kind_of Array
       end
-    end
 
-    it "raises an error if there are not reservations for a specified room and a given date range" do
-      expect{ @front_desk.reservations_by_room_date(3, @date_range) }.must_raise ArgumentError
-    end
-    
+      it "raises an error if there are not reservations for a specified room and a given date range" do
+        expect{ @front_desk.reservations_by_room_date(3, @date_range) }.must_raise ArgumentError
+      end
 
-    describe "Cost_by_reservation" do
       it "returns the total cost for a given reservation" do
-        new_reservation = @front_desk.make_resevation(@date_range)
-        reserv_id = new_reservation.id
+        reserv_id = @new_reservation.id
         cost = @front_desk.cost_by_reservation(reserv_id)
         expect(cost).must_be_close_to 400, 0.01
       end
+
     end
   end
 end
