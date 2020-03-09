1f4b4fcea931cc91be3cf884a427643ac73f4ba8
Angela Nguyen <angethuy@gmail.com>
2020-03-09 00:07:02 -0700

    added date range validation

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -5,6 +5,7 @@ module Hotel
     attr_reader :check_in, :check_out, :nights_spent
 
     def initialize(check_in, check_out)
+      raise ArgumentError, 'Invalid date range given.' unless check_out > check_in
       @check_in = check_in
       @check_out = check_out
       @nights_spent = (@check_out - @check_in) 
@@ -14,7 +15,7 @@ module Hotel
       return (@check_in.year.to_s + @check_in.mon.to_s + @check_in.mday.to_s)
     end
 
-    def collide? (other_check_in, other_check_out)
+    def overlap? (other_check_in, other_check_out)
       return ((@check_in..@check_out).cover? (other_check_in)) || ((@check_in-1...@check_out).cover? (other_check_out))
     end
 
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -4,23 +4,23 @@ describe "DateRange class" do
   describe "initialize" do
 
     before do
-      check_in = Date.parse('2020-08-01')
-      check_out = Date.parse('2020-08-04')
-
-      @dates = Hotel::DateRange.new(check_in, check_out)
+      @dates = Hotel::DateRange.new(Date.new, Date.new + 2)
     end
 
     it "creates an instance of DateRange" do
       expect(@dates).must_be_kind_of Hotel::DateRange
     end
 
+    it "raises an error for invalid dates" do
+      expect{(Hotel::DateRange.new(Date.new+2, Date.new))}.must_raise ArgumentError
+    end
+
     it "returns accurate count of nights spent" do
-      expect(@dates.nights_spent).must_equal 3
+      expect(@dates.nights_spent).must_equal 2
     end
 
     it "returns accurate start Date" do
       expect(@dates.check_in).must_be_kind_of Date
-
     end
 
   end
