1ae6a1073a9f370e779dd2f88b3b972faca974b2
Katie <katie.vandervoort@gmail.com>
2020-03-08 19:07:46 -0700

    added reserve_room_from_block method and tests for it

--- lib/block.rb
+++ lib/block.rb
@@ -6,14 +6,24 @@ require_relative 'reservation.rb'
 
 module Hotel
   class Block < Reservation
-    attr_reader
+    attr_reader :start_date, :end_date, :date_range, :disc_rate, :available_rooms, :reserved_rooms
 
     def initialize(start_date, end_date, collection_of_rooms, disc_rate)
       super(start_date,end_date)
       @date_range = get_date_range(start_date, end_date)
       @collection_of_rooms = collection_of_rooms
       @disc_rate = disc_rate
+      @available_rooms = collection_of_rooms
+      @reserved_rooms = []
     end
+
+    def reserve_room_from_block(room)
+      raise RuntimeError.new("room is not available in hotel block") unless @available_rooms.include?(room)
+
+      @available_rooms.delete(room)
+      @reserved_rooms.push(room)
+    end
+
 
 
   end
