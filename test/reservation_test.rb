36075daa3af79304036b4ab70bb9fdfd589d2885
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-08 22:41:19 -0700

    added a nominal test for reservation_id

--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -4,11 +4,11 @@ require 'date'
 
 describe Hotel::Reservation do
   before do
-    room = Hotel::Room.new(1, 200)
-    start_date = Date.today
-    end_date = start_date + 3
-    date_range = Hotel::DateRange.new(start_date,end_date)
-    @reservation = Hotel::Reservation.new(1, date_range,room)   
+    @room = Hotel::Room.new(1, 200)
+    @start_date = Date.today
+    @end_date = @start_date + 3
+    @date_range = Hotel::DateRange.new(@start_date,@end_date)
+    @reservation = Hotel::Reservation.new(1, @date_range,@room)   
   end
   describe "#initialize" do
     it 'Create an instance of reservation' do
@@ -18,6 +18,10 @@ describe Hotel::Reservation do
     it 'Keep track of reservation_id' do
       expect(@reservation.id).must_equal 1
     end 
+
+    it "raise ArgumentError when id is not a positive integer" do
+      expect{(Hotel::Reservation.new("kdjfkasdjfa",@date_range,@room))}.must_raise ArgumentError
+    end
   end
 
   describe "cost" do
