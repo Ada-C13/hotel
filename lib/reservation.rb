556e2bf8800ae2076cc8f28d79bbf900382f1fb9
Jessica Stone <jstone4815@gmail.com>
2020-03-08 23:36:52 -0700

    add :available writer to Reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,8 +1,8 @@
 module Hotel
   class Reservation
-    attr_reader :date_range, :id, :room_number, :nights, :cost, :available, :hotel_block
+    attr_reader :date_range, :id, :room_number, :nights, :cost, :hotel_block
 
-    attr_accessor :total_cost
+    attr_accessor :total_cost, :available
 
     def initialize(date_range, id = 0, room_number = 0, cost = 0, hotel_block = nil, available = nil)
       @date_range = date_range
