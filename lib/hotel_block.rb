5564fb43eeae7f9e74870e576b441481fcc6343d
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-08 21:51:13 -0700

    finished writing test for hotel_block_test and all passed

--- lib/hotel_block.rb
+++ lib/hotel_block.rb
@@ -3,20 +3,12 @@ module Hotel
     attr_reader :date_range , :rooms , :discounted_rate
     def initialize(date_range, rooms, discounted_rate)
       @date_range = date_range
+      raise ArgumentError.new("A block can contain a maximum of 5 rooms") if rooms.length > 5 
+      raise ArgumentError.new("The hotel block cannot be made without having a room") if rooms.empty?
       @rooms = rooms
-      raise ArgumentError.new("A block can contain a maximum of 5 rooms") if @rooms.length > 5 
-      raise ArgumentError.new("The hotel block cannot be made without having a room") if @rooms.empty?
       @discounted_rate = 0.1
     end
   end
 end
 
-# to create a hotel block
-  # 1- check to see if the the room is available or not with the given date range
-      #- check that within the reservations list 
-      # if the room is overap with the given date range
-          # the room cannot be added to the hotel block
-      # else add the room to the hotel block with the given date range
-
-  # 2
 
--- test/hotel_block_test.rb
+++ test/hotel_block_test.rb
@@ -3,13 +3,33 @@ require 'date'
 
 
 describe Hotel::HotelBlock do
+  before do
+    @room1 = Hotel::Room.new(1, 200)
+    @room2 = Hotel::Room.new(2, 200)
+    @room3 = Hotel::Room.new(3, 200)
+    @room4 = Hotel::Room.new(4, 200)
+    @room5 = Hotel::Room.new(5, 200)
+    @room6 = Hotel::Room.new(6, 200)
+    @room_array1 = [@room1, @room2]
+    @room_array2 = [@room1,@room2,@room3,@room4,@room5,@room6]
+    @room_array3 = []
+    @start_date = Date.today
+    @end_date = @start_date + 3
+    @date_range = Hotel::DateRange.new(@start_date,@end_date)
+    @discount_rate = 0.1 
+  end
   describe "initailize HotelBlcok" do
     it "create an instance " do
-    
+      hotel_block = Hotel::HotelBlock.new(@date_range,@room_array1,@discount_rate) 
+      expect(hotel_block).must_be_kind_of Hotel::HotelBlock
     end
 
     it "raise ArgumentError when the length of rooms_array is greater than 5" do
-      
+      expect{Hotel::HotelBlock.new(@date_range,@room_array2,@discount_rate)}.must_raise ArgumentError
+    end
+
+    it "raise ArgumentError when rooms is an empty array" do
+      expect{Hotel::HotelBlock.new(@date_range,@room_array3,@discount_rate)}.must_raise ArgumentError
     end
   end
 end
